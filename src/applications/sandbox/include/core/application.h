7869349e595b8c4da6d159f730b5dceb37fb18a4
Dave De Breuck <dave.de.breuck@howest.be>
2020-07-28 12:17:21 +0200

    Make sure we can create a window

--- src/applications/sandbox/include/core/application.h
+++ src/applications/sandbox/include/core/application.h
@@ -2,6 +2,11 @@
 
 #include "core/coreapplication.h"
 
+namespace rex
+{
+    class Window;
+}
+
 namespace sandbox
 {
     class Application : public rex::CoreApplication
@@ -9,5 +14,8 @@ namespace sandbox
     public:
         Application();
         ~Application() override;
+
+    protected:
+        std::unique_ptr<rex::Window> createWindow() override;
     };
 }
--- src/applications/sandbox/src/core/application.cpp
+++ src/applications/sandbox/src/core/application.cpp
@@ -1,11 +1,35 @@
 #include "core/entrypoint.h"
 
 #include "core/application.h"
+#include "core/win32window.h"
+
+#include "events/event.h"
 
 //-------------------------------------------------------------------------
 sandbox::Application::Application() = default;
 //-------------------------------------------------------------------------
 sandbox::Application::~Application() = default;
+
+//-------------------------------------------------------------------------
+std::unique_ptr<rex::Window> sandbox::Application::createWindow()
+{
+    const std::string WINDOW_TITLE = "Rex";
+
+    const int WINDOW_WIDTH = 1280;
+    const int WINDOW_HEIGHT = 720;
+
+    rex::WindowProperties properties =
+    {
+        WINDOW_TITLE,
+
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+
+        [this](const rex::events::Event& event) { rex::CoreApplication::onEvent(event); }
+    };
+
+    return std::make_unique<rex::win32::Window>(properties);
+}
 
 //-------------------------------------------------------------------------
 rex::CoreApplication* rex::createApplication()
