178229f141e12039632701cc9629eecc719f22f2
Dave De Breuck <davedebreuck.gamedeveloper@gmail.com>
2020-07-29 00:32:49 +0200

    An example layer

--- src/applications/sandbox/src/core/application.cpp
+++ src/applications/sandbox/src/core/application.cpp
@@ -1,8 +1,33 @@
 #include "core/entrypoint.h"
 #include "core/application.h"
+#include "core/layer.h"
+
+#include "events/event.h"
+
+class ExampleLayer : public rex::engine::Layer 
+{
+public:
+    ExampleLayer()
+        :Layer("Example")
+    {}
+
+    //-------------------------------------------------------------------------
+    void onUpdate() override
+    {
+        // Nothing to implement
+    }
+    //-------------------------------------------------------------------------
+    void onEvent(rex::events::Event& event)
+    {
+        // Nothing to implement
+    }
+};
 
 //-------------------------------------------------------------------------
-sandbox::Application::Application() = default;
+sandbox::Application::Application()
+{
+    pushBackLayer(std::make_unique<ExampleLayer>());
+}
 //-------------------------------------------------------------------------
 sandbox::Application::~Application() = default;
 
