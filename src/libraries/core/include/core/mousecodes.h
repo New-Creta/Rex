2277b455121e75a84dee16cc0903d14d8a613207
Dave De Breuck <davedebreuck.gamedeveloper@gmail.com>
2020-08-02 17:36:19 +0200

    Copy mouse codes from WinUser.h

--- src/libraries/core/include/core/mousecodes.h
+++ src/libraries/core/include/core/mousecodes.h
@@ -4,18 +4,25 @@ namespace rex
 {
     enum class MouseCode
     {
-        LEFT = 0,
-        MIDDLE = 1,
-        RIGHT = 2,
+        LEFT    = 1,
+        RIGHT   = 2,
+        MIDDLE  = 4,
+        X1      = 5,
+        X2      = 6,
     };
 
+    //-------------------------------------------------------------------------
     inline std::ostream& operator<<(std::ostream& os, MouseCode mouseCode)
     {
+        
+
         os << static_cast<uint16>(mouseCode);
         return os;
     }
 }
 
-#define RX_MOUSE_BUTTON_LEFT rex::MouseCode::LEFT
-#define RX_MOUSE_BUTTON_MIDDLE rex::MouseCode::MIDDLE
-#define RX_MOUSE_BUTTON_RIGHT rex::MouseCode::RIGHT
+#define RX_MOUSE_BUTTON_LEFT    rex::MouseCode::LEFT
+#define RX_MOUSE_BUTTON_RIGHT   rex::MouseCode::RIGHT
+#define RX_MOUSE_BUTTON_MIDDLE  rex::MouseCode::MIDDLE
+#define RX_MOUSE_BUTTON_X1      rex::MouseCode::X1
+#define RX_MOUSE_BUTTON_X2      rex::MouseCode::X2
