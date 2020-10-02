a0586d4e26b9f0a84cd03567d15bba42e5c9b662
Dave De Breuck <dave.de.breuck@howest.be>
2020-10-02 21:58:44 +0200

    We changed to static libraries

--- src/libraries/core/include/rex_core_global.h
+++ src/libraries/core/include/rex_core_global.h
@@ -1,10 +1,18 @@
 #ifndef REX_CORE_GLOBAL_H
 #define REX_CORE_GLOBAL_H
-    
-#ifdef REX_CORE_LIB
-# define REX_CORE_EXPORT __declspec(dllexport)
+
+#ifdef _WINDOWS
+    #if REX_DYNAMIC_LINK
+        #ifdef REX_CORE_LIB
+            # define REX_CORE_EXPORT __declspec(dllexport)
+        #else
+            # define REX_CORE_EXPORT __declspec(dllimport)
+        #endif
+    #else
+        #define REX_CORE_EXPORT
+    #endif
 #else
-# define REX_CORE_EXPORT __declspec(dllimport)
+    #error We only support windows
 #endif
 
 #endif // REX_CORE_GLOBAL_H
--- src/libraries/graphics/include/rex_graphics_global.h
+++ src/libraries/graphics/include/rex_graphics_global.h
@@ -1,10 +1,18 @@
 #ifndef REX_GRAPHICS_GLOBAL_H
 #define REX_GRAPHICS_GLOBAL_H
 
-#ifdef REX_GRAPHICS_LIB
-# define REX_GRAPHICS_EXPORT __declspec(dllexport)
+#ifdef _WINDOWS
+    #if REX_DYNAMIC_LINK
+        #ifdef REX_GRAPHICS_LIB
+            # define REX_GRAPHICS_EXPORT __declspec(dllexport)
+        #else
+            # define REX_GRAPHICS_EXPORT __declspec(dllimport)
+        #endif
+    #else
+        #define REX_GRAPHICS_EXPORT
+    #endif
 #else
-# define REX_GRAPHICS_EXPORT __declspec(dllimport)
+    #error We only support windows
 #endif
 
 #endif // REX_GRAPHICS_GLOBAL_H
