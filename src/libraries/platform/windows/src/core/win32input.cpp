1372f87929991337e6bcd8581a8dc5ab2615a3dc
Dave De Breuck <dave.de.breuck@howest.be>
2020-10-07 00:58:48 +0200

    implement win32 input manager

--- src/libraries/platform/windows/src/core/win32input.cpp
+++ src/libraries/platform/windows/src/core/win32input.cpp
@@ -1,71 +1,77 @@
 #include "core/win32input.h"
+#include "core/win32inputcontroller.h"
 #include "core/win32application.h"
 #include "core/win32window.h"
 
 //-------------------------------------------------------------------------
+rex::win32::InputImpl::InputImpl()
+{
+    m_controller = std::make_unique<InputController>();
+}
+//-------------------------------------------------------------------------
+rex::win32::InputImpl::~InputImpl() = default;
+
+//-------------------------------------------------------------------------
+void rex::win32::InputImpl::poll()
+{
+    m_controller->update();
+}
+
+//-------------------------------------------------------------------------
 bool rex::win32::InputImpl::isKeyPressed(KeyCode code) const
 {
-    UNUSED_PARAM(code);
-
-    //auto application = static_cast<Application*>(CoreApplication::getInstance());
-    //auto window = static_cast<Window*>(application->getWindow());
-
-    return false;
+    return m_controller->isKeyPressed(code);
+}
+//-------------------------------------------------------------------------
+bool rex::win32::InputImpl::isKeyDown(KeyCode code) const
+{
+    return m_controller->isKeyDown(code);
 }
 //-------------------------------------------------------------------------
 bool rex::win32::InputImpl::isKeyReleased(KeyCode code) const
 {
-    UNUSED_PARAM(code);
-
-    //auto application = static_cast<Application*>(CoreApplication::getInstance());
-    //auto window = static_cast<Window*>(application->getWindow());
-
-    return false;
+    return m_controller->isKeyReleased(code);
+}
+//-------------------------------------------------------------------------
+bool rex::win32::InputImpl::isKeyUp(KeyCode code) const
+{
+    return m_controller->isKeyUp(code);
 }
 
 //-------------------------------------------------------------------------
 bool rex::win32::InputImpl::isMouseButtonPressed(MouseCode button) const
 {
-    UNUSED_PARAM(button);
-
-    //auto application = static_cast<Application*>(CoreApplication::getInstance());
-    //auto window = static_cast<Window*>(application->getWindow());
-
-    return false;
+    return m_controller->IsMouseButtonPressed(button);
+}
+//-------------------------------------------------------------------------
+bool rex::win32::InputImpl::isMouseButtonDown(MouseCode button) const
+{
+    return m_controller->IsMouseButtonDown(button);
 }
 //-------------------------------------------------------------------------
 bool rex::win32::InputImpl::isMouseButtonReleased(MouseCode button) const
 {
-    UNUSED_PARAM(button);
-
-    //auto application = static_cast<Application*>(CoreApplication::getInstance());
-    //auto window = static_cast<Window*>(application->getWindow());
-
-    return false;
+    return m_controller->IsMouseButtonReleased(button);
+}
+//-------------------------------------------------------------------------
+bool rex::win32::InputImpl::isMouseButtonUp(MouseCode button) const
+{
+    return m_controller->IsMouseButtonUp(button);
 }
 
 //-------------------------------------------------------------------------
-rex::Point rex::win32::InputImpl::getMousePosition() const
+rex::Point64 rex::win32::InputImpl::getMousePosition(bool previousFrame) const
 {
-    //auto application = static_cast<Application*>(CoreApplication::getInstance());
-    //auto window = static_cast<Window*>(application->getWindow());
-
-    return { 0, 0 };
+    return m_controller->getMousePosition(previousFrame);
 }
 
 //-------------------------------------------------------------------------
-unsigned int rex::win32::InputImpl::getMouseX() const
+int64 rex::win32::InputImpl::getMouseX(bool previousFrame) const
 {
-    //auto application = static_cast<Application*>(CoreApplication::getInstance());
-    //auto window = static_cast<Window*>(application->getWindow());
-
-    return 0;
+    return m_controller->getMousePosition(previousFrame).x;
 }
 //-------------------------------------------------------------------------
-unsigned int rex::win32::InputImpl::getMouseY() const
+int64 rex::win32::InputImpl::getMouseY(bool previousFrame) const
 {
-    //auto application = static_cast<Application*>(CoreApplication::getInstance());
-    //auto window = static_cast<Window*>(application->getWindow());
-
-    return 0;
+    return m_controller->getMousePosition(previousFrame).y;
 }
