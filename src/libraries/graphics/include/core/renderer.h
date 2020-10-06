a76f08be9f46d363150f979a91a2519ecb5e4164
Dave De Breuck <dave.de.breuck@howest.be>
2020-10-06 18:00:43 +0200

    Implement the renderer

--- src/libraries/graphics/include/core/renderer.h
+++ src/libraries/graphics/include/core/renderer.h
@@ -1,22 +1,39 @@
 #pragma once
 
+#include "rex_graphics_global.h"
+
+#include "core/rendererapi.h"
+
 namespace rex
 {
     namespace graphics
     {
-        enum class RendererAPI
-        {
-            NONE = 0, 
-            OPENGL = 1
-        };
+        class Mesh;
+        class OrthographicCamera;
 
         class Renderer
-        {
+        {   
         public:
-            inline static RendererAPI getAPI() { return s_renderer_api; }
+            REX_GRAPHICS_EXPORT static void initialize(RendererAPI::Type type);
+            REX_GRAPHICS_EXPORT static void destroy();
+
+            REX_GRAPHICS_EXPORT static void beginScene(OrthographicCamera& camera);
+            REX_GRAPHICS_EXPORT static void endScene();
+
+            REX_GRAPHICS_EXPORT static void submit(Mesh* mesh);
+            REX_GRAPHICS_EXPORT static void flush();
+
+            inline static RendererAPI::Type getAPI() { return s_renderer_api->getAPI(); }
 
         private:
-            static RendererAPI s_renderer_api;
+            struct SceneData
+            {
+                glm::mat4 m_viewprojection_matrix;
+            };
+
+            static std::unique_ptr<RendererAPI>     s_renderer_api;
+            static std::unique_ptr<SceneData>       s_scene_data;
+            static bool                             s_initialized;
         };
     }
 }
--- src/libraries/graphics/src/core/renderer.cpp
+++ src/libraries/graphics/src/core/renderer.cpp
@@ -1,6 +1,58 @@
 #include "rex_graphics_pch.h"
+
+#include "core/orthographiccamera.h"
 #include "core/renderer.h"
+#include "core/shader.h"
+#include "core/mesh.h"
 
 //-------------------------------------------------------------------------
-rex::graphics::RendererAPI rex::graphics::Renderer::s_renderer_api = rex::graphics::RendererAPI::OPENGL;
+std::unique_ptr<rex::graphics::RendererAPI> rex::graphics::Renderer::s_renderer_api = nullptr;
+//-------------------------------------------------------------------------
+std::unique_ptr<rex::graphics::Renderer::SceneData> rex::graphics::Renderer::s_scene_data = nullptr;
 
+//-------------------------------------------------------------------------
+bool rex::graphics::Renderer::s_initialized = false;
+
+//-------------------------------------------------------------------------
+void rex::graphics::Renderer::initialize(RendererAPI::Type type)
+{
+    s_renderer_api = rex::graphics::RendererAPI::create(type);
+    s_scene_data = std::make_unique<SceneData>();
+
+    s_initialized = true;
+}
+//-------------------------------------------------------------------------
+void rex::graphics::Renderer::destroy()
+{
+    s_renderer_api.reset();
+
+    s_initialized = false;
+}
+
+//-------------------------------------------------------------------------
+void rex::graphics::Renderer::beginScene(OrthographicCamera& camera)
+{
+    s_scene_data->m_viewprojection_matrix = camera.getViewProjectionMatrix();
+
+    s_renderer_api->clear();
+}
+//-------------------------------------------------------------------------
+void rex::graphics::Renderer::endScene()
+{
+    flush();
+}
+
+//-------------------------------------------------------------------------
+void rex::graphics::Renderer::submit(Mesh* mesh)
+{
+    mesh->getShader()->bind();
+    mesh->getShader()->uploadMat4("u_ViewProjection", s_scene_data->m_viewprojection_matrix);
+
+    s_renderer_api->drawIndexed(mesh->getShader(), mesh->getVertexArray());
+}
+
+//-------------------------------------------------------------------------
+void rex::graphics::Renderer::flush()
+{
+    s_renderer_api->flush();
+}
