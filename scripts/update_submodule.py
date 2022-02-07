9658054be8088f64e5093f68efac947f08e8ed4e
Dave De Breuck <dave.debreuck@twikit.com>
2022-02-07 21:39:31 +0100

    Update submodule
    Retrieve the external directory to update the submodules

--- scripts/update_submodule.py
+++ scripts/update_submodule.py
@@ -1,13 +1,14 @@
 import os
+import repository_directories
 
 if __name__ == "__main__":
     # working directory where we start our script
-    cwd = os.getcwd()        
+    externals_directory = repository_directories.get_externals_directory()        
 
     # add new submodules here that need to be updated when we run this script
     # current submodules updated
     #       * rex-stl
-    submodules = [cwd + "/../externals/rex-stl"]
+    submodules = [externals_directory + "\\rex-stl"]
 
     for submodule in submodules:
         # print the submodule that needs to be updated
