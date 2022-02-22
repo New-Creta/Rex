aa2f9b91458852dde9855623ad469f97a27524ba
Dave De Breuck <dave.debreuck@twikit.com>
2022-02-23 07:36:18 +0800

    Precompiled headers
    Add standard headers and rex-stl header to the pch

--- source/include/2_engine/engine/engine_pch.h
+++ source/include/2_engine/engine/engine_pch.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include <type_traits>
+#include <string>
+
+#include "rex_stl/types.h"
+#include "rex_stl/utilities/yes_no_enum.h"
--- source/include/3_platform/windows/windows_pch.h
+++ source/include/3_platform/windows/windows_pch.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
+#include "rex_stl/types.h"
+#include "rex_stl/diagnostics/assert.h"
+#include "rex_stl/utilities/yes_no_enum.h"
