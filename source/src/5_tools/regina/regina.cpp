a50d7a3be9e11bda02693e4a2c571046fab668ad
Dave De Breuck <dave.debreuck@twikit.com>
2022-05-02 18:45:33 +0200

    Regina
    Update regina end point.

--- source/src/5_tools/regina/regina.cpp
+++ source/src/5_tools/regina/regina.cpp
@@ -3,12 +3,26 @@
 #include "win32_window.h"
 #include "regina.h"
 
-//-----------------------------------------------------------------------
-rex::CoreApplication* rex::create_application()
+namespace tags
 {
+    const std::string WINDOW_WIDTH = "xres";
+    const std::string WINDOW_HEIGHT = "yres";
+}
+
+//-----------------------------------------------------------------------
+rex::CoreApplication* rex::create_application(const ApplicationArguments& arguments)
+{
+    std::string s_window_width = arguments.get_argument_value(tags::WINDOW_WIDTH);
+    std::string s_window_height = arguments.get_argument_value(tags::WINDOW_WIDTH);
+
     rex::ApplicationDescription description;
 
-    description.max_fps = 60u;
+    description.name = "rex";
+    description.window_width = !s_window_width.empty() ? std::stoi(s_window_width) : 1280;
+    description.window_height = !s_window_height.empty() ? std::stoi(s_window_height) : 720;
+
+    // R_ASSERT(description.window_width > 16);
+    // R_ASSERT(description.window_height > 16);
 
     return new regina::Regina(description);
 }
