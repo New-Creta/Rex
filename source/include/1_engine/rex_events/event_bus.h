50436ffd65756a6f6ad9413eb5f10734e3693d8a
Dave De Breuck <dave.debreuck@twikit.com>
2022-05-02 18:38:52 +0200

    Event System
    Proposal event system API

---
+++ source/include/1_engine/rex_events/event_bus.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "patterns/singleton.h"
+
+#include "event_queue.h"
+
+namespace rex
+{
+    namespace events
+    {
+        class EventBusImpl
+        {
+        public:
+            using Activate = rtl::YesNoEnum;
+
+            EventBusImpl();
+            ~EventBusImpl();
+
+            EventQueue* get_active_queue() const;
+
+            size_t push_event_queue(EventQueue* queue, Activate activate = Activate::No);
+
+            void activate_event_queue(size_t queueID);
+            void deactivate_event_queue(size_t queueID);
+
+            void disable_all();
+
+        private:
+            std::vector<EventQueue*> m_queues;
+            EventQueue* m_active_queue;
+        };
+
+        class EventBus : public StaticSingleton<EventBusImpl>
+        {
+        };
+    }
+}
---
+++ source/include/1_engine/rex_events/rex_events_pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <functional>
+#include <type_traits>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <memory>
+
+#include "rex_stl/types.h"
+#include "rex_stl/utilities/yes_no_enum.h"
