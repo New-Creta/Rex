2d366da1af3d2197164fca960b58c8abe34b62cd
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2022-05-03 18:45:44 +0200

    Precompiled headers

--- source/include/1_engine/rex_engine/rex_engine_pch.h
+++ source/include/1_engine/rex_engine/rex_engine_pch.h
@@ -5,3 +5,6 @@
 
 #include "rex_stl/types.h"
 #include "rex_stl/utilities/yes_no_enum.h"
+#include "rex_stl/utilities/output_param.h"
+#include "rex_stl/memory/unique_ptr.h"
+#include "rex_stl/containers/vector.h"
