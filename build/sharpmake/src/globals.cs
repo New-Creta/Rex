216dc3d8ec04baaf45b64d932968c3e16f8a2753
Nick De Breuck <nick.debreuck@outlook.com>
2023-01-17 22:16:20 +0000

    integrated rex stl changes into rex and added rexstl as a submodule for rex

---
+++ build/sharpmake/src/globals.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.Json;
+
+[module: Sharpmake.Reference("System.Text.Json.dll")]
+
+public class Globals
+{
+  static readonly private string folder_in_root = "source";
+  static private string root;
+  static private string source_root;
+  static private string thirdparty_root;
+  static private string sharpmake_root;
+  static private string tools_root;
+  static private string libs_root;
+
+  static public string Root
+  {
+    get
+    {
+      return root;
+    }
+  }
+  static public string SourceRoot
+  {
+    get
+    {
+      return source_root;
+    }
+  }
+  static public string ThirdpartyRoot
+  {
+    get
+    {
+      return thirdparty_root;
+    }
+  }
+  static public string SharpmakeRoot
+  {
+    get
+    {
+      return sharpmake_root;
+    }
+  }
+  static public string ToolsRoot
+  {
+    get
+    {
+      return tools_root;
+    }
+  }
+  static public string LibsRoot
+  {
+    get
+    {
+      return libs_root;
+    }
+  }
+
+  static public void Init()
+  {
+    string current_directory = Directory.GetCurrentDirectory();
+
+    while (Directory.GetDirectories(current_directory).ToList().FindIndex(x => Path.GetFileName(x) == folder_in_root) == -1)
+    {
+      if (Directory.GetDirectoryRoot(current_directory) == current_directory)
+      {
+        throw new System.Exception("Failed to find root directory");
+      }
+      current_directory = Directory.GetParent(current_directory).FullName;
+    }
+
+
+    root = current_directory;
+
+    string settings_json_path = Path.Combine(root, "build", "config", "settings.json");
+    string json_blob = File.ReadAllText(settings_json_path);
+    Dictionary<string, string> settings = JsonSerializer.Deserialize<Dictionary<string, string>>(json_blob);
+
+
+    source_root = Path.Combine(root, settings["source_folder"]);
+    thirdparty_root = Path.Combine(source_root, "0_thirdparty");
+    sharpmake_root = Path.Combine(root, "build", "sharpmake");
+    tools_root = Path.Combine(root, settings["intermediate_folder"], settings["tools_folder"]);
+    libs_root = Path.Combine(root, settings["intermediate_folder"], settings["libs_folder"]);
+    System.Console.WriteLine($"Root path:{root}");
+  }
+}
---
+++ build/sharpmake/src/target.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using Sharpmake;
+
+public class RexTarget : ITarget
+{
+  public DevEnv DevEnv;
+  public Platform Platform;
+  public Config Config;
+  public Compiler Compiler;
+  public DotNetFramework DotNetFramework = DotNetFramework.v4_8;
+  public RexTarget()
+  { }
+  public RexTarget(Platform platform, DevEnv devEnv, Config config, Compiler compiler = Compiler.MSVC)
+  {
+    DevEnv = devEnv;
+    Platform = platform;
+    Config = config;
+    Compiler = compiler;
+  }
+
+  // This is the display name of the configuration dropdown in visual studio
+  public override string Name
+  {
+    get
+    {
+      string config_str = string.Concat(Config.ToString().ToString().Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString()));
+
+      return (config_str).ToLowerInvariant();
+    }
+  }
+
+  public Optimization Optimization
+  {
+    get { return ConfigManager.get_optimization_for_config(Config); }
+  }
+
+  public static RexTarget[] GetDefaultTargets()
+  {
+    return new RexTarget[]
+    { 
+      GetVsOnlyTarget(),
+      new RexTarget(Platform.win64, DevEnv.ninja, Config.debug | Config.debug_opt | Config.release, Compiler.MSVC | Compiler.Clang)
+    };
+  }
+  public static RexTarget GetVsOnlyTarget()
+  {
+    return new RexTarget(Platform.win64, DevEnv.vs2019, Config.debug | Config.debug_opt | Config.release, Compiler.MSVC);
+  }
+
+  public static RexTarget GetCoverageTarget()
+  {
+    return new RexTarget(Platform.win64, DevEnv.ninja, Config.coverage, Compiler.Clang);
+  }
+
+  public static RexTarget GetAsanTarget()
+  {
+    return new RexTarget(Platform.win64, DevEnv.ninja, Config.address_sanitizer, Compiler.Clang);
+  }
+  public static RexTarget GetUBsanTarget()
+  {
+    return new RexTarget(Platform.win64, DevEnv.ninja, Config.undefined_behavior_sanitizer, Compiler.Clang);
+  }
+  public static RexTarget GetFuzzyTarget()
+  {
+    return new RexTarget(Platform.win64, DevEnv.ninja, Config.fuzzy, Compiler.Clang);
+  }
+
+}
+
