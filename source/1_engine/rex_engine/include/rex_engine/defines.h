9d80f4a56e7cb0e24ee4b445fd3d25e95ff2ecf5
Dave De Breuck <dave.debreuck@twikit.com>
2023-01-30 12:01:36 +0100

    Some defines that tell the compiler this is an unused parameter

---
+++ source/1_engine/rex_engine/include/rex_engine/defines.h
@@ -0,0 +1,23 @@
+#ifndef REX_DEFINES
+#define REX_DEFINES
+
+#if defined _MSC_VER
+#define UNUSED_PARAM(...)                                                                                                                                                                                                                            \
+        {                                                                                                                                                                                                                                                \
+            __VA_ARGS__;                                                                                                                                                                                                                                 \
+        }
+#elif defined __clang__
+#define UNUSED_PARAM(...)                                                                                                                                                                                                                            \
+        _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wunused-value\"")                                                                                                                                                          \
+        {                                                                                                                                                                                                                                                \
+            __VA_ARGS__;                                                                                                                                                                                                                                 \
+        }                                                                                                                                                                                                                                                \
+        _Pragma("clang diagnostic pop")
+#elif defined __GNUC__
+#define UNUSED_PARAM(...)                                                                                                                                                                                                                            \
+        {                                                                                                                                                                                                                                                \
+            __VA_ARGS__;                                                                                                                                                                                                                                 \
+        }
+#endif
+
+#endif
