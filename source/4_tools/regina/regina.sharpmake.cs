0b1eecdaefb5bf3e04e4d85e51183c985b7296c8
Dave De Breuck <dave.debreuck@twikit.com>
2023-01-30 12:03:36 +0100

    Add windows dependencies on windows platforms Add windows subsystem linker option on windows platforms

--- source/4_tools/regina/regina.sharpmake.cs
+++ source/4_tools/regina/regina.sharpmake.cs
@@ -4,23 +4,30 @@ using System.IO;
 [Generate]
 public class Regina : ToolsProject
 {
-  public Regina() : base()
-  {
-    // The name of the project in Visual Studio. The default is the name of
-    // the class, but you usually want to override that.
-    Name = "Regina";
-    GenerateTargets();
+    public Regina() : base()
+    {
+        // The name of the project in Visual Studio. The default is the name of
+        // the class, but you usually want to override that.
+        Name = "Regina";
+        GenerateTargets();
 
-    string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
-    SourceRootPath = ThisFileFolder;
-  }
+        string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
+        SourceRootPath = ThisFileFolder;
+    }
 
-  public override void Configure(RexConfiguration conf, RexTarget target)
-  {
-    base.Configure(conf, target);
+    public override void Configure(RexConfiguration conf, RexTarget target)
+    {
+        base.Configure(conf, target);
 
-    conf.Output = Configuration.OutputType.Exe;
+        conf.Output = Configuration.OutputType.Exe;
 
-    conf.add_dependency<RexApplicationCore>(target);
-  }
+        switch (target.Platform)
+        {
+            case Platform.win32:
+            case Platform.win64:
+                conf.add_dependency<RexWindows>(target);
+                conf.Options.Add(Options.Vc.Linker.SubSystem.Windows);
+                break;
+        }
+    }
 }
