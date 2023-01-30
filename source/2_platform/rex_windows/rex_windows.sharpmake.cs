d93ed3a2cf6abc47219b4efa58ffc26e5a05e538
Dave De Breuck <dave.debreuck@twikit.com>
2023-01-30 12:03:59 +0100

    Add renderer dependency

--- source/2_platform/rex_windows/rex_windows.sharpmake.cs
+++ source/2_platform/rex_windows/rex_windows.sharpmake.cs
@@ -4,26 +4,26 @@ using System.IO;
 [Generate]
 public class RexWindows : PlatformProject
 {
-  public RexWindows() : base()
-  {
-    // The name of the project in Visual Studio. The default is the name of
-    // the class, but you usually want to override that.
-    Name = "RexWindows";
-    GenerateTargets();
+    public RexWindows() : base()
+    {
+        // The name of the project in Visual Studio. The default is the name of
+        // the class, but you usually want to override that.
+        Name = "RexWindows";
+        GenerateTargets();
 
-    string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
-    SourceRootPath = ThisFileFolder;
-  }
+        string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
+        SourceRootPath = ThisFileFolder;
+    }
 
-  public override void Configure(RexConfiguration conf, RexTarget target)
-  {
-    base.Configure(conf, target);
+    public override void Configure(RexConfiguration conf, RexTarget target)
+    {
+        base.Configure(conf, target);
 
-    conf.Output = Configuration.OutputType.Lib;
+        conf.Output = Configuration.OutputType.Lib;
 
-    conf.add_dependency<RexEngine>(target);
-    conf.add_dependency<RexRenderer>(target);
+        conf.add_dependency<RexEngine>(target);
+        conf.add_dependency<RexRenderer>(target);
 
-    conf.add_public_define("REX_PLATFORM_WINDOWS");
-  }
+        conf.add_public_define("REX_PLATFORM_WINDOWS");
+    }
 }
