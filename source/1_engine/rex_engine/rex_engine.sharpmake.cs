94343a99c5c08efcb75dfee281ad34a94e24fd8b
Dave De Breuck <dave.debreuck@twikit.com>
2023-01-30 12:03:49 +0100

    Specify debug macro

--- source/1_engine/rex_engine/rex_engine.sharpmake.cs
+++ source/1_engine/rex_engine/rex_engine.sharpmake.cs
@@ -4,23 +4,34 @@ using System.IO;
 [Generate]
 public class RexEngine : EngineProject
 {
-  public RexEngine() : base()
-  {
-    // The name of the project in Visual Studio. The default is the name of
-    // the class, but you usually want to override that.
-    Name = "RexEngine";
-    GenerateTargets();
+    public RexEngine() : base()
+    {
+        // The name of the project in Visual Studio. The default is the name of
+        // the class, but you usually want to override that.
+        Name = "RexEngine";
+        GenerateTargets();
 
-    string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
-    SourceRootPath = ThisFileFolder;
-  }
+        string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
+        SourceRootPath = ThisFileFolder;
+    }
 
-  public override void Configure(RexConfiguration conf, RexTarget target)
-  {
-    base.Configure(conf, target);
+    public override void Configure(RexConfiguration conf, RexTarget target)
+    {
+        base.Configure(conf, target);
 
-    conf.Output = Configuration.OutputType.Lib;
+        conf.Output = Configuration.OutputType.Lib;
 
-    conf.add_dependency<RexStdExtra>(target);
-  }
+        conf.add_dependency<RexStdExtra>(target);
+
+        switch (target.Config)
+        {
+            case Config.debug:
+            case Config.debug_opt:
+            case Config.address_sanitizer:
+            case Config.undefined_behavior_sanitizer:
+            case Config.fuzzy:
+                conf.add_public_define("REX_DEBUG");
+                break;
+        }
+    }
 }
