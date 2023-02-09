ea6c42ecf74015c52d66f6a7ff0b3350ad7c8215
Dave De Breuck <dave.debreuck@twikit.com>
2023-02-09 10:14:43 +0100

    Remove developer comments

--- source/1_engine/rex_engine/src/diagnostics/hr_call.cpp
+++ source/1_engine/rex_engine/src/diagnostics/hr_call.cpp
@@ -1,15 +1,3 @@
-// ============================================
-//
-// REX GAME ENGINE
-//
-// Author: Nick De Breuck
-// Twitter: @nick_debreuck
-//
-// File: hr_call.cpp
-// Copyright (c) Nick De Breuck 2023
-//
-// ============================================
-
 #include "rex_engine/diagnostics/win/hr_call.h"
 
 #include "rex_engine/diagnostics/logging.h"
