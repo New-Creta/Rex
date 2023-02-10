b959b147ce8d8379bb539da3e6e16db93854a58d
DaveDeBreuck <dave.debreuck@twikit.com>
2023-02-10 12:38:25 +0100

    Renderer Init: Create GLM sharpmake

---
+++ source/0_thirdparty/glm/glm.sharpmake.cs
@@ -0,0 +1,30 @@
+using Sharpmake;
+using System.IO;
+using System.Diagnostics;
+
+[Generate]
+public class GLM : BasicCPPProject
+{
+  public GLM() : base()
+  {
+    // The name of the project in Visual Studio. The default is the name of
+    // the class, but you usually want to override that.
+    Name = GenerateName("GLM");
+    GenerateTargets();
+
+    // The directory that contains the source code we want to build is the
+    // same as this one. This string essentially means "the directory of
+    // the script you're reading right now."
+    string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
+    SourceRootPath = ThisFileFolder;
+  }
+
+  public override void Configure(RexConfiguration conf, RexTarget target)
+  {
+    base.Configure(conf, target);
+
+    conf.IncludePaths.Add(SourceRootPath);
+    conf.Output = Configuration.OutputType.Lib;
+    conf.Options.Remove(Options.Vc.General.TreatWarningsAsErrors.Enable);
+  }
+}
