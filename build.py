05b6c6c92583b034eb612de283fb51c5b01b6888
Nick De Breuck <nick.debreuck@outlook.com>
2023-02-14 01:02:02 +0000

    BOYSCOUT: build.py can now build based on project, config and compiler

--- build.py
+++ build.py
@@ -40,36 +40,14 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   
   parser.add_argument("-clean", help="clean all intermediate files first", action="store_true")
+  parser.add_argument("-project", help="project to build")
   parser.add_argument("-config", help="configuration to build for")
-  parser.add_argument("-unittests", help="generate unit tests", action="store_true")
-  parser.add_argument("-coverage", help="generate coverage", action="store_true")
-  parser.add_argument("-asan", help="generate address sanitizer", action="store_true")
-  parser.add_argument("-ubsan", help="generate undefined behavior sanitizer", action="store_true")
-  parser.add_argument("-fuzzy", help="generate fuzzy testing", action="store_true")
+  parser.add_argument("-compiler", help="configuration to build with")
 
   args, unknown = parser.parse_known_args()
-
-  ninja_files = __all_ninja_files(rexpy.util.find_root())
-
-  build_any_special_tests = args.coverage or args.asan or args.ubsan or args.fuzzy
-
-  if not args.coverage:
-    ninja_files = [file for file in ninja_files if "_coverage" not in file]
-  if not args.asan:
-    ninja_files = [file for file in ninja_files if "_asan" not in file]
-  if not args.ubsan:
-    ninja_files = [file for file in ninja_files if "_ubsan" not in file]
-  if not args.fuzzy:
-    ninja_files = [file for file in ninja_files if "_fuzzy" not in file]
-  if build_any_special_tests == True and not args.unittests:
-    ninja_files = [file for file in ninja_files if __is_special_test_file(file)]
-
-  result = 0
-
-  for ninja_file in ninja_files:
-    result |= rexpy.build.new_build(ninja_file, args.clean)
+  result = rexpy.build.new_build(args.project, args.config, args.compiler, args.clean)
 
   if result != 0:
-    rexpy.diagnostics.log_err("Not all builds succeeded")
+    rexpy.diagnostics.log_err("Build failed")
   else:
     rexpy.diagnostics.log_info("Build successful")
