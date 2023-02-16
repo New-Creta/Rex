00a3c113a8864076c30ec4552fda3a8d21dd73f2
Dave De Breuck <dave.debreuck@twikit.com>
2023-02-16 18:37:51 +0100

    Renderer Initialization Review: Boyscout ClearState Move Rendererinfo into an opengl namespace

--- source/2_platform/rex_opengl/src/opengl_renderer.cpp
+++ source/2_platform/rex_opengl/src/opengl_renderer.cpp
@@ -3,6 +3,7 @@
 #include "rex_opengl/opengl_error.h"
 
 #include "rex_std/string.h"
+#include "rex_std_extra/math/color.h"
 
 #if REX_PLATFORM_X64
 #include <glad/gl.h>
@@ -27,28 +28,43 @@ namespace rex
   //-------------------------------------------------------------------------
   struct ClearState
   {
-    f32 rgba[4];
+    rsl::Color4f rgba;
     f32 depth;
     u8  stencil;
     u32 flags;
   };
 
-  RendererInfo s_renderer_info;
-  ClearState s_clear_state{ 0.0f, 0.3f, 0.2f, 1.0f, 1.0f, 0x00, REX_CLEAR_COLOUR_BUFFER };
+  //-------------------------------------------------------------------------
+  ClearState make_default_clear_state()
+  {
+    ClearState cs;
+
+    cs.rgba = rsl::colors::MediumSeaGreen;
+    cs.depth = 1.0f;
+    cs.stencil = 0x00;
+    cs.flags = REX_CLEAR_COLOUR_BUFFER;
+
+    return cs;
+  }
 
   namespace renderer
   {
+    namespace opengl
+    {
+      RendererInfo s_renderer_info;
+    }
+
     //-------------------------------------------------------------------------
     // general accessors
     const RendererInfo& get_info()
     {
-      return s_renderer_info;
+      return opengl::s_renderer_info;
     }
 
     //-------------------------------------------------------------------------
-    const char8* shader_platform()
+    const ShaderPlatform shader_platform()
     {
-      return "glsl";
+      return ShaderPlatform::GLSL;
     }
 
     //-------------------------------------------------------------------------
@@ -65,7 +81,8 @@ namespace rex
 
     namespace backend
     {
-      GLint s_backbuffer_fbo = -1;
+      ClearState s_clear_state = make_default_clear_state();
+      s32 s_backbuffer_fbo = -1;
 
       //-------------------------------------------------------------------------
       bool initialize()
@@ -76,10 +93,10 @@ namespace rex
         const GLubyte* gl_renderer = glGetString(GL_RENDERER);
         const GLubyte* gl_vendor = glGetString(GL_VENDOR);
 
-        s_renderer_info.shader_version = rsl::small_stack_string((const char8*)glsl_version);
-        s_renderer_info.api_version = rsl::small_stack_string((const char8*)gl_version);
-        s_renderer_info.renderer = rsl::small_stack_string((const char8*)gl_renderer);
-        s_renderer_info.vendor = rsl::small_stack_string((const char8*)gl_vendor);
+        opengl::s_renderer_info.shader_version = rsl::small_stack_string((const char8*)glsl_version);
+        opengl::s_renderer_info.api_version = rsl::small_stack_string((const char8*)gl_version);
+        opengl::s_renderer_info.renderer = rsl::small_stack_string((const char8*)gl_renderer);
+        opengl::s_renderer_info.vendor = rsl::small_stack_string((const char8*)gl_vendor);
 
         // gles base fbo is not 0
         glGetIntegerv(GL_FRAMEBUFFER_BINDING, &s_backbuffer_fbo);
@@ -102,13 +119,14 @@ namespace rex
       //-------------------------------------------------------------------------
       void end_frame()
       {
-        // unused on this platform
+        // Nothing to implement
       }
 
-      // clears
       void clear()
       {
         ClearState& cs = s_clear_state;
+
+        REX_ASSERT(cs.flags);
 
         GL_CALL(glClearDepth(cs.depth));
         GL_CALL(glClearStencil(cs.stencil));
@@ -125,13 +143,7 @@ namespace rex
           GL_CALL(glStencilMask(0xff));
         }
 
-        GL_CALL(glClearColor(cs.rgba[0], cs.rgba[1], cs.rgba[2], cs.rgba[3]));
-
-        if (!cs.flags)
-        {
-          return;
-        }
-
+        GL_CALL(glClearColor(cs.rgba.red, cs.rgba.green, cs.rgba.blue, cs.rgba.alpha));
         GL_CALL(glClear(cs.flags));
       }
 
