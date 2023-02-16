55ba812459b8f3155404eec784329f63469f80fc
Dave De Breuck <dave.debreuck@twikit.com>
2023-02-16 18:35:43 +0100

    Renderer Initialization Review: Add no discard Add static rex todo

--- source/2_platform/rex_opengl/include/rex_opengl/resources/shader.h
+++ source/2_platform/rex_opengl/include/rex_opengl/resources/shader.h
@@ -2,10 +2,12 @@
 
 #include "rex_engine/types.h"
 
+#include "rex_std/bonus/attributes.h"
+
 namespace rex
 {
-    u32 create_vertex_shader(u64 shaderElementCount, const char** shaderElements, s32* shaderElementLength = nullptr);
-    u32 create_fragment_shader(u64 shaderElementCount, const char** shaderElements, s32* shaderElementLength = nullptr);
+    REX_NO_DISCARD u32 create_vertex_shaders(u64 shaderElementCount, const char** shaderElements, s32* shaderElementLength = nullptr);
+    REX_NO_DISCARD u32 create_fragment_shaders(u64 shaderElementCount, const char** shaderElements, s32* shaderElementLength = nullptr);
 
     void destroy_vertex_shader(u32 shader);
     void destroy_fragment_shader(u32 shader);
