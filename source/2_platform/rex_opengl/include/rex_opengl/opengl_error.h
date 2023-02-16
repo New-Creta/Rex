b99a8b9955e41a0d693b206766bf8b29635ed06e
Dave De Breuck <dave.debreuck@twikit.com>
2023-02-16 18:37:05 +0100

    Renderer Initialization Review: Put opengl error functions into an opengl namespace

--- source/2_platform/rex_opengl/include/rex_opengl/opengl_error.h
+++ source/2_platform/rex_opengl/include/rex_opengl/opengl_error.h
@@ -9,63 +9,67 @@
 #error "Unsupported platform"
 #endif
 
-#if defined _MSC_VER
+#if defined REX_COMPILER_MSVC
     #pragma warning( push )
-    #pragma warning( disable : 4505 )
+    #pragma warning( disable : 4505 ) // 'function' : unreferenced local function has been removed
 #endif
 
 namespace rex
 {
+    namespace opengl
+    {
 #ifdef REX_DEBUG
-    //-------------------------------------------------------------------------
-    inline char8* gl_get_error_message(u32 error)
-    {
-        switch (error)
+        //-------------------------------------------------------------------------
+        inline char8* get_error_message(u32 error)
         {
-        case GL_INVALID_OPERATION: return "Invalid Operation";
-        case GL_INVALID_ENUM: return "Invalid Enum";
-        case GL_INVALID_VALUE: return "Invalid Value";
-        case GL_INVALID_INDEX: return "Invalid Index";
-        case GL_INVALID_FRAMEBUFFER_OPERATION: return "Invalid Framebuffer Operation";
+            switch (error)
+            {
+            case GL_INVALID_OPERATION: return "Invalid Operation";
+            case GL_INVALID_ENUM: return "Invalid Enum";
+            case GL_INVALID_VALUE: return "Invalid Value";
+            case GL_INVALID_INDEX: return "Invalid Index";
+            case GL_INVALID_FRAMEBUFFER_OPERATION: return "Invalid Framebuffer Operation";
+            }
+
+            return "Unknown error";
         }
 
-        return "Unknown error";
-    }
-
-    //-------------------------------------------------------------------------
-    inline u32 gl_check_error()
-    {
-        return glGetError();
-    }
-    //-------------------------------------------------------------------------
-    inline bool gl_log_call(const char* function, const char* file, int32 line)
-    {
-        u32 error = gl_check_error();
-        if (error == GL_NO_ERROR)
+        //-------------------------------------------------------------------------
+        inline u32 get_current_error()
         {
-            return true;
+            // This wil also clear the error with GL_NO_ERROR when the value is retrieved.
+            return glGetError();
         }
-
-        while (error != GL_NO_ERROR)
+        //-------------------------------------------------------------------------
+        inline bool log_call(const char* function, const char* file, int32 line)
         {
-            REX_ERROR("[OpenGL Error] ({0}, {1}): {2} {3}: {4}", error, gl_get_error_message(error), function, file, line);
-            
-            error = gl_check_error();
-        }
+            u32 error = get_current_error();
+            if (error == GL_NO_ERROR)
+            {
+                return true;
+            }
 
-        return false;
-    }
+            while (error != GL_NO_ERROR)
+            {
+                REX_ERROR("[OpenGL Error] ({0}, {1}): {2} {3}: {4}", error, get_error_message(error), function, file, line);
+                
+                error = get_current_error();
+            }
+
+            return false;
+        }
 #endif
+    }
 }
 
 #ifdef REX_DEBUG
-    #define GL_CALL(x) rex::gl_check_error();\
+    #define GL_CALL(x) rex::opengl::get_current_error();\
 		    x; \
-		    if (!rex::gl_log_call(#x, __FILE__, __LINE__)) DEBUG_BREAK();
+		    if (!rex::opengl::log_call(#x, __FILE__, __LINE__)) DEBUG_BREAK();
 #else
     #define GL_CALL(x) x
 #endif
 
-#if defined _MSC_VER
+#if defined REX_COMPILER_MSVC
     #pragma warning( pop )
 #endif
