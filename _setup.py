7e2fc7c5f7c5403f559d9a1475c17f7cef7bd04f
Dave De Breuck <dave.debreuck@twikit.com>
2023-02-21 00:45:36 +0100

    Issue 32: Download rexpy and rexstd

--- _setup.py
+++ _setup.py
@@ -8,24 +8,21 @@
 #
 # ============================================
 
-# this is the only file that can't use rexpy.
-# as this will install rexpy if it's not yet installed.
-
 import os
 import argparse
 import pkg_resources
 import shutil
+import subprocess
+import sys
 
 def is_rexpy_installed():
-  installed = {pkg.key for pkg in pkg_resources.working_set}
-  if "rexpy" in installed:
-    return True
-
-  return False
+  return os.path.exists(os.path.join(os.getcwd(), "rexpy"))
+def is_rexstd_installed():
+  return os.path.exists(os.path.join(os.getcwd(), os.path.join("..", os.path.join("..", os.path.join("source", os.path.join("0_thirdparty", "rex_std"))))))
 
 def install_rexpy(forceInstall):
   # first set our working directory to rexpy.
-  # this way the package will be installed in {root}/build/scripts/rexpy
+  # this way the repository will be downloaded in {root}/build/scripts/rexpy
   # this keeps the root clean.
 
   # first let's make sure we're in the root directory
@@ -39,24 +36,62 @@ def install_rexpy(forceInstall):
   if not build_exists or not source_exists:
     print("Error: You're not running setup.py from the root directory. Please run this from the root directory and try again")
     return
+  
+  # rexpy is located in build/scripts
+  cwd = os.getcwd()
+  new_wd = os.path.join(cwd, "build", "scripts")
+  os.chdir(new_wd)
 
   if forceInstall == False and is_rexpy_installed():
     print(f"rexpy is already installed - skipping install")
     return
+  elif forceInstall == True:
+    if is_rexpy_installed():
+      print(f"remove the already install rexpy")
+      shutil.rmtree("rexpy")
 
-  # rexpy is located in build/scripts/rexpy
+  # # now run the install script.
+  p = subprocess.Popen(["python.exe", os.path.join(new_wd, 'fetch_rexpy.py')], stdout=sys.stdout)
+  p.communicate()
+
+  # reset the working directory
+  os.chdir(cwd)
+
+def install_rexstd(forceInstall):
+  # first set our working directory to rexstd.
+  # this way the repository will be downloaded in {root}/source/0_thirdparty/rexstd
+  # this keeps the root clean.
+
+  # first let's make sure we're in the root directory
+  # we don't have access to rexstd yet, we'll have to manually
+  # check if we're in the root.
+
+  # let's assume if "build" and "source"are found, we're in the root
+  build_exists = os.path.exists("./build")
+  source_exists = os.path.exists("./source")
+
+  if not build_exists or not source_exists:
+    print("Error: You're not running setup.py from the root directory. Please run this from the root directory and try again")
+    return
+
+  # rexstd is located in build/scripts
   cwd = os.getcwd()
-  new_wd = os.path.join(cwd, "build", "scripts", "rexpy")
+  new_wd = os.path.join(cwd, "build", "scripts")
   os.chdir(new_wd)
 
-  # now run the install script.
-  os.system(f"py {os.path.join(new_wd, 'install.py')} install")
+  if forceInstall == False and is_rexstd_installed():
+    print(f"rexstd is already installed - skipping install")
+    return
+  elif forceInstall == True:
+    if is_rexstd_installed():
+      print(f"remove the already install rexstd")
+      shutil.rmtree("../../source/0_thirdparty/rex_std")
 
-  # this will result in a few new folders generated, which we can safely remove
-  # these folders are "build", "dist" and "rexpy.egg-info"
-  shutil.rmtree("build")
-  shutil.rmtree("dist")
-  shutil.rmtree("rexpy.egg-info")
+  # # now run the install script.
+  # os.system(f"py {os.path.join(new_wd, 'fetch_rexstd.py')} " + cwd)
+  args = "-cwd="+cwd
+  p = subprocess.Popen(["python.exe", os.path.join(new_wd, 'fetch_rexstd.py'), args], stdout=sys.stdout)
+  p.communicate()
 
   # reset the working directory
   os.chdir(cwd)
@@ -64,12 +99,14 @@ def install_rexpy(forceInstall):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("-update_rexpy", help="update rexpy, this is basically a reinstall", action="store_true")
+  parser.add_argument("-update_rexstd", help="update rexstd, this is basically a reinstall", action="store_true")
 
   args, unknown = parser.parse_known_args()
 
   install_rexpy(args.update_rexpy)
+  install_rexstd(args.update_rexstd)
 
-  # now that rexpy is installed, we can safely call the rest of the code
+  # now that rexpy and rexstd is installed, we can safely call the rest of the code
 
   arguments_to_pass_on = ""
   for arg in unknown:
