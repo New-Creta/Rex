062c6088cd0763f229edd7670fdd9d1b4b4a2457
DaveDeBreuck <dave.debreuck@twikit.com>
2023-02-20 17:04:14 +0100

    Issue 23: Move everything REX_RENDERER_OPENGL specific into a different file Adjust the API within gui_application.cpp

--- source/2_platform/rex_windows/src/gui_application.cpp
+++ source/2_platform/rex_windows/src/gui_application.cpp
@@ -6,6 +6,7 @@
 #include "rex_engine/frameinfo/fps.h"
 #include "rex_engine/frameinfo/frameinfo.h"
 #include "rex_renderer_core/renderer.h"
+#include "rex_renderer_core/context.h"
 #include "rex_std/bonus/utility/scopeguard.h"
 #include "rex_std/math.h"
 #include "rex_std/memory.h"
@@ -16,127 +17,6 @@
 #include <Windows.h>
 
 // NOLINTBEGIN(modernize-use-nullptr,-warnings-as-errors)
-
-#if REX_RENDERER_OPENGL
-  #include <glad/gl.h>
-  #include <glad/wgl.h>
-
-struct WglContext
-{
-  HDC dc;
-  HGLRC glrc;
-};
-
-WglContext g_glctx; // NOLINT (fuchsia-statically-constructed-objects,-warnings-as-errors, cppcoreguidelines-avoid-non-const-global-variables,-warnings-as-errors)
-
-namespace rex
-{
-  bool rex_make_gl_context_current()
-  {
-    return static_cast<bool>(wglMakeCurrent(g_glctx.dc, g_glctx.glrc));
-  }
-  void rex_gl_swap_buffers()
-  {
-    SwapBuffers(g_glctx.dc);
-  }
-
-  namespace opengl
-  {
-    void release_device_context(HWND hwnd, HDC dc)
-    {
-      // The application must call the ReleaseDC function for each call to the GetWindowDC function
-      // and for each call to the GetDC function that retrieves a common DC.
-      // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-releasedc
-      ReleaseDC(hwnd, dc);
-    }
-
-    bool create_gl_context(HWND hwnd)
-    {
-      g_glctx.dc = GetDC(hwnd);
-
-      PIXELFORMATDESCRIPTOR pfd;
-      memset(&pfd, 0, sizeof(pfd));
-
-      pfd.nSize        = sizeof(PIXELFORMATDESCRIPTOR);
-      pfd.nVersion     = 1;
-      pfd.dwFlags      = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
-      pfd.iPixelType   = PFD_TYPE_RGBA;
-      pfd.cColorBits   = 32;
-      pfd.cAlphaBits   = 8;
-      pfd.cDepthBits   = 24;
-      pfd.cStencilBits = 8;
-      pfd.iLayerType   = PFD_MAIN_PLANE;
-
-      const s32 pf = ChoosePixelFormat(g_glctx.dc, &pfd);
-      if(pf == NULL || SetPixelFormat(g_glctx.dc, pf, &pfd) == FALSE)
-      {
-        release_device_context(hwnd, g_glctx.dc);
-        REX_ERROR("Failed to set a compatible pixel format");
-        return false;
-      }
-
-      HGLRC temp = wglCreateContext(g_glctx.dc);
-      if(temp == NULL)
-      {
-        release_device_context(hwnd, g_glctx.dc);
-        REX_ERROR("Failed to create the initial rendering context");
-        return false;
-      }
-
-      wglMakeCurrent(g_glctx.dc, temp);
-
-      // Load WGL Extensions:
-      gladLoaderLoadWGL(g_glctx.dc);
-
-      rsl::array attribs = {WGL_CONTEXT_MAJOR_VERSION_ARB,
-                            3, // Set the MAJOR version of OpenGL to 3
-                            WGL_CONTEXT_MINOR_VERSION_ARB,
-                            2, // Set the MINOR version of OpenGL to 2
-                            WGL_CONTEXT_FLAGS_ARB,
-                            WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB, // Set our OpenGL context to be forward compatible
-                            0};
-
-      const char* extensions = wglGetExtensionsStringARB(g_glctx.dc);
-      if(strstr(extensions, "WGL_ARB_create_context") == NULL)
-      {
-        g_glctx.glrc = wglCreateContextAttribsARB(g_glctx.dc, NULL, attribs.data());
-        if(g_glctx.glrc == NULL)
-        {
-          wglDeleteContext(temp);
-          release_device_context(hwnd, g_glctx.dc);
-          REX_ERROR("Failed to create Windows OpenGL Context");
-
-          return false;
-        }
-
-        wglMakeCurrent(NULL, NULL);
-        wglDeleteContext(temp);
-        wglMakeCurrent(g_glctx.dc, g_glctx.glrc);
-      }
-      else
-      {
-        g_glctx.glrc = temp;
-      }
-
-      if(gladLoaderLoadGL() == NULL)
-      {
-        wglMakeCurrent(NULL, NULL);
-        wglDeleteContext(g_glctx.glrc);
-        release_device_context(hwnd, g_glctx.dc);
-        REX_ERROR("Glad Loader failed!");
-
-        return false;
-      }
-
-      return true;
-    }
-  } // namespace opengl
-} // namespace rex
-
-  #define CREATE_CTX(hwnd) rex::opengl::create_gl_context(hwnd)
-#else
-  #define CREATE_CTX(hwnd) (hwnd == nullptr)
-#endif
 
 namespace rex
 {
@@ -160,13 +40,12 @@ namespace rex
         }
         subscribe_window_events();
 
-        HWND hwnd = static_cast<HWND>(window->primary_display_handle());
-        if(CREATE_CTX(hwnd) == 0)
+        if(context::create(window->primary_display_handle()) == false)
         {
           return false;
         }
 
-        if(renderer::initialize((void*)&hwnd, gui_params.max_render_commands) == false) // NOLINT (google-readability-casting,-warnings-as-errors, readability-simplify-boolean-expr,-warnings-as-errors)
+        if(renderer::initialize(nullptr, gui_params.max_render_commands) == false)
         {
           return false;
         }
