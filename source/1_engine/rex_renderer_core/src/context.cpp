9bf4f7212babcd9ac0a865c58d638acf25984c3d
Nick De Breuck <nick.debreuck@outlook.com>
2023-03-01 01:53:53 +0000

    Boyscout Testing: Applying clang tidy checks

--- source/1_engine/rex_renderer_core/src/context.cpp
+++ source/1_engine/rex_renderer_core/src/context.cpp
@@ -4,40 +4,40 @@
 
 namespace rex
 {
-    namespace context
+  namespace context
+  {
+    bool create(void* userData)
     {
-        bool create(void* userData)
-        {
-            bool result = true;
+      bool result = true;
 
-            result &= os::pre_setup(userData);
-            if(!result)
-            {
-                os::rollback(userData);
+      result &= os::pre_setup(userData);
+      if(!result)
+      {
+        os::rollback(userData);
 
-                REX_ERROR("Context OS setup failed!");
-                return false;
-            }
+        REX_ERROR("Context OS setup failed!");
+        return false;
+      }
 
-            result &= backend::create(userData);
-            if(!result)
-            {
-                os::rollback(userData);
+      result &= backend::create(userData);
+      if(!result)
+      {
+        os::rollback(userData);
 
-                REX_ERROR("Context backend initialization failed!");
-                return false;
-            }
+        REX_ERROR("Context backend initialization failed!");
+        return false;
+      }
 
-            result &= os::post_setup(userData);
-            if(!result)
-            {
-                os::rollback(userData);
+      result &= os::post_setup(userData);
+      if(!result)
+      {
+        os::rollback(userData);
 
-                REX_ERROR("Context OS setup failed!");
-                return false;
-            }
+        REX_ERROR("Context OS setup failed!");
+        return false;
+      }
 
-            return result;
-        }
+      return result;
     }
-}
+  } // namespace context
+} // namespace rex
