0d8cdf9f9931aead98e3995f6c9a50ce8124778d
Nick De Breuck <nick.debreuck@outlook.com>
2023-03-18 22:03:19 +0000

    [development/filesystem] added some comments into regina.cpp on how the memory tracking should work. this needs to be removed later

--- source/4_tools/regina/src/regina.cpp
+++ source/4_tools/regina/src/regina.cpp
@@ -24,6 +24,44 @@ namespace rex
 
   ApplicationCreationParams app_entry(PlatformCreationParams&& platformParams, CommandLineArguments&& cmdArgs)
   {
+    // the following pointer should get tracked and automatically be processed by the tracking system
+    // depending on which one is enabled
+    
+    // the code will follow the following step:
+    // operator new -> Global Allocator (Tracked) -> Global Allocator (Untracked) -> System Call
+    //                                                                                    |
+    //                                                                                    | 
+    // operator new <- Global Allocator (Tracked) <- Global Allocator (Untracked)  <------+ 
+    //                         |
+    //                         |
+    //                         v
+    //               Save to file/Send to tool
+    // 
+    // Functionality should be implemented in memory_tracking.cpp
+    // However, we'll need a file system first to save things to disk
+    // Later we can add a network layer to send data over the network to a different tool
+    // This tool will use the rex engine as well as a backbone.
+    // The tool will open a socket and will listen to requests coming in on that socket
+    // 
+    // Requirements of this tool:
+    // - Open a memory report file saved by the engine (See above)
+    // - Track all live allocations with their callstacks
+    // - Diff between different points in time
+    // - Track all single frame allocations
+   
+
+    rsl::unique_ptr<int> p = rsl::make_unique<int>(1);
+
+
+
+
+
+
+
+
+
+
+
     ApplicationCreationParams app_params(rsl::move(platformParams), rsl::move(cmdArgs));
 
     app_params.gui_params.window_width  = 1280;
