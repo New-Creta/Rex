d0fa1e8ed51a77297c581ba7664a9e091630e7b7
Nick De Breuck <nick.debreuck@outlook.com>
2023-03-18 22:02:52 +0000

    [development/filesystem] removed heap allocation from static initialization, this is forbidden

--- source/1_engine/rex_engine/src/memory/memory_tracking.cpp
+++ source/1_engine/rex_engine/src/memory/memory_tracking.cpp
@@ -1,26 +1,9 @@
+#include "rex_engine/diagnostics/logging.h"
+#include "rex_engine/filesystem/filesystem.h"
 #include "rex_engine/memory/memory_tracking.h"
 #include "rex_engine/frameinfo/frameinfo.h"
 #include "rex_std/iostream.h"
 #include "rex_std/limits.h"
-
-//  // we need to have an internal error reporting here
-//  // so that we don't allocate heap memory when overflowing
-//  // as this would cause a stack overflow
-#ifdef REX_ENABLE_MEM_TRACKING
-  #define REX_HEAP_TRACK_ERR(cond, msg)                                                                                                                                                                                                                  \
-    if(!(cond))                                                                                                                                                                                                                                          \
-    {                                                                                                                                                                                                                                                    \
-      rsl::cout << "Err: " << msg << "\n";                                                                                                                                                                                                               \
-    }
-  #define REX_HEAP_TRACK_WARN(cond, msg)                                                                                                                                                                                                                  \
-    if(!(cond))                                                                                                                                                                                                                                          \
-    {                                                                                                                                                                                                                                                    \
-      rsl::cout << "Warn: " << msg << "\n";                                                                                                                                                                                                               \
-    }
-#else
-#define REX_HEAP_TRACK_ERR(cond, msg)
-#define REX_HEAP_TRACK_WARN(cond, msg)
-#endif
 
 namespace rex
 {
@@ -45,30 +28,43 @@ namespace rex
   void MemoryTracker::initialize(rsl::memory_size maxMemUsage)
   {
     m_max_mem_usage = rsl::high_water_mark<s64>(static_cast<s64>(maxMemUsage));
+    m_is_initialized = true;
   }
 
   void MemoryTracker::track_alloc(void* /*mem*/, MemoryHeader* header)
   {
+    REX_ASSERT_X(m_is_initialized, "Trying to track allocation but memory tracker isn't initialized yet");
+
     const rsl::unique_lock lock(m_mem_tracking_mutex);
-    m_mem_usage += header->size().size_in_bytes();
-    m_usage_per_tag[rsl::enum_refl::enum_integer(header->tag())] += header->size().size_in_bytes();
-    REX_HEAP_TRACK_ERR(m_mem_usage.value() <= m_max_mem_usage, "Using more memory than allowed! usage: " << m_mem_usage.value() << " max: " << m_max_mem_usage);
+    increment_mem_usage(header->size().size_in_bytes());
+    increment_mem_tag_usage(header->tag(), header->size().size_in_bytes());
+    
+    save_to_file(header, AllocOp::Allocation);
+
+    REX_ERROR_X(m_mem_usage.value() <= m_max_mem_usage, "Using more memory than allowed! usage: {} max: {}", m_mem_usage.value(), m_max_mem_usage);
   }
   void MemoryTracker::track_dealloc(void* /*mem*/, MemoryHeader* header)
   {
+    REX_ASSERT_X(m_is_initialized, "Trying to track allocation but memory tracker isn't initialized yet");
+
     const rsl::unique_lock lock(m_mem_tracking_mutex);
-    REX_HEAP_TRACK_WARN(header->frame_index() != frame_info().index(), "Memory freed in the same frame it's allocated");
-    m_mem_usage -= header->size().size_in_bytes();
-    m_usage_per_tag[rsl::enum_refl::enum_integer(header->tag())] -= header->size().size_in_bytes();
+    decrement_mem_usage(header->size().size_in_bytes());
+    decrement_mem_tag_usage(header->tag(), header->size().size_in_bytes());
+
+    save_to_file(header, AllocOp::Deallocation);
   }
 
   void MemoryTracker::push_tag(MemoryTag tag)
   {
+    REX_ASSERT_X(m_is_initialized, "Trying to track allocation but memory tracker isn't initialized yet");
+
     const rsl::unique_lock lock(m_mem_tag_tracking_mutex);
     thread_local_memory_tag_stack()[++thread_local_mem_tag_index()] = tag;
   }
   void MemoryTracker::pop_tag()
   {
+    REX_ASSERT_X(m_is_initialized, "Trying to track allocation but memory tracker isn't initialized yet");
+
     const rsl::unique_lock lock(m_mem_tag_tracking_mutex);
     --thread_local_mem_tag_index();
   }
@@ -83,6 +79,43 @@ namespace rex
     const rsl::unique_lock lock(m_mem_tracking_mutex);
     MemoryTracker::UsagePerTag usage_per_tag_copy = m_usage_per_tag;
     return usage_per_tag_copy;
+  }
+
+  void MemoryTracker::increment_mem_usage(card64 size)
+  {
+    m_mem_usage += size;
+  }
+
+  void MemoryTracker::increment_mem_tag_usage(MemoryTag tag, card64 size)
+  {
+    m_usage_per_tag[rsl::enum_refl::enum_integer(tag)] += size;
+
+  }
+
+  void MemoryTracker::decrement_mem_usage(card64 size)
+  {
+    m_mem_usage -= size;
+  }
+
+  void MemoryTracker::decrement_mem_tag_usage(MemoryTag tag, card64 size)
+  {
+    m_usage_per_tag[rsl::enum_refl::enum_integer(tag)] -= size;
+  }
+
+  void MemoryTracker::save_to_file(MemoryHeader* header, AllocOp op)
+  {
+    // Serialize the header and save it to disk
+    // Alloc Operation
+    // Ptr
+    // Size
+    // Thread ID
+    // Tag
+    // Frame Idx
+    // Callstack
+
+    rsl::array<rsl::byte, 100> dummy_array{};
+    vfs::save_to_file("dummy_file", &op, sizeof(AllocOp));
+    vfs::save_to_file("dummy_file", header, sizeof(MemoryHeader));
   }
 
   MemoryTracker& mem_tracker()
