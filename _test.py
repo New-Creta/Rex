eeb1cad1c3dd799a061cc0c969314440cc7b191f
Nick De Breuck <nick.debreuck@outlook.com>
2023-04-10 16:57:18 +0100

    [test_branch] test_commit

--- _test.py
+++ _test.py
@@ -51,6 +51,7 @@ if __name__ == "__main__":
     regis.test.test_fuzzy_testing()
   if args.all or args.auto_test:
     
+    
     if args.auto_test_timeout:
       auto_test_timeout_secs = args.auto_test_timeout
 
