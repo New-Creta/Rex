f6c2cfc8996421eb26c727403445e2c68015ff32
Dave De Breuck <dave.debreuck@twikit.com>
2023-04-15 22:58:07 +0200

    [development/directx] DirectX includes DXGI includes

---
+++ source/2_platform/rex_directx/include/rex_directx/directx_util.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <d3d12.h>
+#include <d3d11on12.h>
+
+#pragma comment(lib, "d3d12.lib")
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
---
+++ source/2_platform/rex_directx/include/rex_directx/dxgi/util.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <dxgi1_6.h>
+
+#include <dxgicommon.h>
+#include <dxgidebug.h>
+
+#pragma comment(lib, "dxgi.lib")
