f6592a1686db628ad0ba01af53510094983f1222
Dave De Breuck <dave.debreuck@twikit.com>
2023-04-15 22:57:33 +0200

    [development/directx] Directx Feature Level DirectX Feature Shader Model

---
+++ source/2_platform/rex_directx/include/rex_directx/directx_feature_level.h
@@ -0,0 +1,20 @@
+#pragma once 
+
+#include "rex_engine/types.h"
+
+#include "rex_std/string.h"
+
+struct IDXGIAdapter;
+
+namespace rex
+{
+    struct FeatureLevelInfo
+    {
+        s32         level;
+        rsl::string name;
+    };
+
+    bool is_correct_feature_level(s32 level);
+
+    FeatureLevelInfo query_feature_level(IDXGIAdapter* adapter);
+}
