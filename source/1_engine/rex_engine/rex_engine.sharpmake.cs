9f19d8bc44a1254b9855b1ca0712ce6b333349e8
Dave De Breuck <dave.debreuck@twikit.com>
2023-04-15 23:24:09 +0200

    [development/directx] Specify graphics API in engine and windows module

--- source/1_engine/rex_engine/rex_engine.sharpmake.cs
+++ source/1_engine/rex_engine/rex_engine.sharpmake.cs
@@ -74,7 +74,18 @@ public class RexEngine : EngineProject
         break;
     }
 
-    conf.add_public_define("REX_API_OPENGL");
+    switch (GenerateSettings.GraphicsAPI)
+    {
+        case GraphicsAPI.OpenGL:
+            conf.add_public_define("REX_API_OPENGL");
+            break;
+        case GraphicsAPI.DirectX12:
+            conf.add_public_define("REX_API_DIRECTX12");
+            break;
+        default:
+            break;
+    }
+
     conf.add_public_define("REXLOG_COMPILED_LIB");
   }
 
