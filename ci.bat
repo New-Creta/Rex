758325ec559045f0e720673031f2f5b107e3d2f3
Nick De Breuck <nick.debreuck@outlook.com>
2023-04-15 18:58:11 +0100

    [devops/async_ci] removing ci.bat file

--- ci.bat
+++
@@ -1,9 +0,0 @@
-py _setup.py
-py _generate.py
-py _build.py -project=regina -compiler=msvc -config=debug
-py _build.py -project=regina -compiler=msvc -config=debug_opt
-py _build.py -project=regina -compiler=msvc -config=release
-py _build.py -project=regina -compiler=clang -config=debug
-py _build.py -project=regina -compiler=clang -config=debug_opt
-py _build.py -project=regina -compiler=clang -config=release
-@REM py _test.py -iwyu -clang_tidy -unit_tests -asan -ubsan
