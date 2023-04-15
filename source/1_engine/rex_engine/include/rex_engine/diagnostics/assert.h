ce324f62f7e344a62d34e51a235dcc2bc2479b53
Nick De Breuck <nick.debreuck@outlook.com>
2023-04-15 23:43:35 +0100

    [development/assert_system] added rex_assert function

--- source/1_engine/rex_engine/include/rex_engine/diagnostics/assert.h
+++ source/1_engine/rex_engine/include/rex_engine/diagnostics/assert.h
@@ -2,5 +2,10 @@
 
 #include "rex_std/assert.h"
 
+namespace rex
+{
+  void rex_assert(const rsl::fmt_stack_string& msg);
+}
+
 #define REX_ASSERT(...)         RSL_ASSERT(__VA_ARGS__)
 #define REX_ASSERT_X(cond, ...) RSL_ASSERT_X(cond, __VA_ARGS__)
---
+++ source/1_engine/rex_engine/src/diagnostics/assert.cpp
@@ -0,0 +1,8 @@
+#include "rex_engine/diagnostics/assert.h"
+
+namespace rex
+{
+  void rex_assert(const rsl::fmt_stack_string& msg)
+  {
+  }
+}
