a668a47b19ac1c3bfa6259b16227a0c94839d752
DaveDeBreuck <dave.debreuck@twikit.com>
2023-05-09 14:44:34 +0200

    [development/event_system_new_creta] Update the state controller to store an enum instead of an unsigned integer

--- source/1_engine/rex_engine/include/rex_engine/state_controller.h
+++ source/1_engine/rex_engine/include/rex_engine/state_controller.h
@@ -11,34 +11,35 @@ namespace rex
   {
   public:
     StateController()
-        : m_state(0)
+        : m_state(static_cast<TState>(0))
     {
     }
     explicit StateController(TState state)
-        : m_state(static_cast<u32>(state))
+        : m_state(state)
     {
     }
 
     void add_state(TState state)
     {
-      m_state = m_state | static_cast<u32>(state);
+      m_state = static_cast<TState>(static_cast<u32>(m_state) | static_cast<u32>(state));
     }
     void remove_state(TState state)
     {
-      m_state = m_state & ~static_cast<u32>(state);
+      m_state = static_cast<TState>(static_cast<u32>(m_state) & ~static_cast<u32>(state));
     }
     void change_state(TState state)
     {
-      m_state = 0;
+      m_state = static_cast<TState>(0);
+
       add_state(state);
     }
 
     bool has_state(TState state) const
     {
-      return (m_state & static_cast<u32>(state)) != 0u;
+      return (static_cast<u32>(m_state) & static_cast<u32>(state)) != 0u;
     }
 
   private:
-    u32 m_state;
+    TState m_state;
   };
 } // namespace rex
