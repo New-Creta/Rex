9a4d166c729ff5327d50d1ecc0a86c48fc66258e
Nick De Breuck <nick.debreuck@outlook.com>
2023-05-14 14:38:34 +0100

    [development/input] applying clang-tidy auto fixes

--- source/2_platform/rex_windows/include/rex_windows/input/input.h
+++ source/2_platform/rex_windows/include/rex_windows/input/input.h
@@ -12,5 +12,5 @@ namespace rex
     bool is_key_down(char8 key);
     bool is_key_released(char8 key);
     bool is_key_up(char8 key);
-  }
-}
+  } // namespace input
+} // namespace rex
--- source/2_platform/rex_windows/src/input/input.cpp
+++ source/2_platform/rex_windows/src/input/input.cpp
@@ -1,10 +1,9 @@
 #include "rex_windows/input/input.h"
 
-#include "rex_std/vector.h"
-#include "rex_std/assert.h"
-#include "rex_std/algorithm.h"
-
 #include "rex_engine/diagnostics/win/win_call.h"
+#include "rex_std/algorithm.h"
+#include "rex_std/assert.h"
+#include "rex_std/vector.h"
 
 #define NOMINMAX
 #include <Windows.h>
@@ -13,20 +12,20 @@ namespace rex
 {
   namespace input
   {
-    card32 g_current_input_idx = 1;
+    card32 g_current_input_idx               = 1;
     rsl::array<bool, 256> g_keys_even_frames = {};
-    rsl::array<bool, 256> g_keys_odd_frames = {};
-    rsl::array<bool, 256>* g_prev_keys = &g_keys_even_frames;
-    rsl::array<bool, 256>* g_current_keys = &g_keys_odd_frames;
+    rsl::array<bool, 256> g_keys_odd_frames  = {};
+    rsl::array<bool, 256>* g_prev_keys       = &g_keys_even_frames;
+    rsl::array<bool, 256>* g_current_keys    = &g_keys_odd_frames;
 
     uint8 poll()
     {
       constexpr card32 max_input_keys = (rsl::numeric_limits<uint8>::max)();
-      for (card32 i = g_current_input_idx; i < max_input_keys; ++i, ++g_current_input_idx)
+      for(card32 i = g_current_input_idx; i < max_input_keys; ++i, ++g_current_input_idx)
       {
         uint8 key = static_cast<uint8>(i);
 
-        if (WIN_CALL(GetAsyncKeyState(key)) & 0x8000)
+        if(WIN_CALL(GetAsyncKeyState(key)) & 0x8000)
         {
           ++g_current_input_idx;
           return key;
@@ -42,9 +41,9 @@ namespace rex
       rsl::swap(g_prev_keys, g_current_keys);
       g_current_keys->fill(false);
 
-      while (char8 key = input::poll())
+      while(char8 key = input::poll())
       {
-        if (key == '\0')
+        if(key == '\0')
           break;
 
         g_current_keys->at(key) = true;
@@ -67,5 +66,5 @@ namespace rex
     {
       return g_current_keys->at(key) == false;
     }
-  }
-}
+  } // namespace input
+} // namespace rex
