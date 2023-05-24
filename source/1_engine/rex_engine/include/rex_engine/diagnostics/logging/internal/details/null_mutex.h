9d2622687c165e1445b54fd8a1a997c924b60059
Dave De Breuck <dave.debreuck@twikit.com>
2023-05-24 12:42:44 +0200

    [development/diagnostics_remove_inc_files] Remove legacy code

--- source/1_engine/rex_engine/include/rex_engine/diagnostics/logging/internal/details/null_mutex.h
+++ source/1_engine/rex_engine/include/rex_engine/diagnostics/logging/internal/details/null_mutex.h
@@ -18,33 +18,5 @@ namespace rexlog
       void lock() const {}
       void unlock() const {}
     };
-
-    struct NullAtomicInt
-    {
-      int value;
-      NullAtomicInt() = default;
-
-      explicit NullAtomicInt(int newValue)
-          : value(newValue)
-      {
-      }
-
-      int load(rsl::memory_order /*unused*/ = rsl::memory_order::relaxed) const
-      {
-        return value;
-      }
-
-      void store(int newValue, rsl::memory_order /*unused*/ = rsl::memory_order::relaxed)
-      {
-        value = newValue;
-      }
-
-      int exchange(int newValue, rsl::memory_order /*unused*/ = rsl::memory_order::relaxed)
-      {
-        rsl::swap(newValue, value);
-        return newValue; // return value before the call
-      }
-    };
-
   } // namespace details
 } // namespace rexlog
