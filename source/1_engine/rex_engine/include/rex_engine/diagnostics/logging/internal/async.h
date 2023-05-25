dc741162ada8d509b3f06e1bbc042124f0fb7cfb
Dave De Breuck <dave.debreuck@twikit.com>
2023-05-25 21:13:28 +0200

    [development/diagnostics_remove_inc_files] Remove unused code

--- source/1_engine/rex_engine/include/rex_engine/diagnostics/logging/internal/async.h
+++ source/1_engine/rex_engine/include/rex_engine/diagnostics/logging/internal/async.h
@@ -45,10 +45,9 @@ namespace rexlog
             auto& registry_inst = details::Registry::instance();
 
             // create global thread pool if not already exists..
+            const rsl::unique_lock<rsl::recursive_mutex> tp_lock(registry_inst.tp_mutex());
 
-            auto& mutex = registry_inst.tp_mutex();
-            const rsl::unique_lock<rsl::recursive_mutex> tp_lock(mutex);
-            auto tp = registry_inst.get_tp();
+            auto tp = registry_inst.tp();
             if (tp == nullptr)
             {
                 tp = rsl::allocate_shared<details::ThreadPool>(rex::global_debug_allocator(), details::g_default_async_q_size, 1U);
@@ -61,42 +60,4 @@ namespace rexlog
             return rsl::shared_ptr<AsyncLogger>(rsl::move(new_logger));
         }
     };
-
-    using async_factory = AsyncFactoryImpl<AsyncOverflowPolicy::Block>;
-    using async_factory_nonblock = AsyncFactoryImpl<AsyncOverflowPolicy::OverrunOldest>;
-
-    template <typename Sink, typename... SinkArgs>
-    inline rsl::shared_ptr<rexlog::Logger> create_async(rex::DebugString loggerName, SinkArgs&&... sinkArgs)
-    {
-        return async_factory::create<Sink>(rsl::move(loggerName), rsl::forward<SinkArgs>(sinkArgs)...);
-    }
-
-    template <typename Sink, typename... SinkArgs>
-    inline rsl::shared_ptr<rexlog::Logger> create_async_nb(rex::DebugString loggerName, SinkArgs&&... sinkArgs)
-    {
-        return async_factory_nonblock::create<Sink>(rsl::move(loggerName), rsl::forward<SinkArgs>(sinkArgs)...);
-    }
-
-    // set global thread pool.
-    inline void init_thread_pool(size_t qSize, size_t threadCount, rsl::function<void()> onThreadStart, rsl::function<void()> onThreadStop)
-    {
-        auto tp = rsl::allocate_shared<details::ThreadPool>(rex::global_debug_allocator(), qSize, threadCount, onThreadStart, onThreadStop);
-        details::Registry::instance().set_tp(rsl::move(tp));
-    }
-
-    inline void init_thread_pool(size_t qSize, size_t threadCount, rsl::function<void()> onThreadStart)
-    {
-        init_thread_pool(qSize, threadCount, rsl::move(onThreadStart), [] {});
-    }
-
-    inline void init_thread_pool(size_t qSize, size_t threadCount)
-    {
-        init_thread_pool(qSize, threadCount, [] {}, [] {});
-    }
-
-    // get the global thread pool.
-    inline rsl::shared_ptr<rexlog::details::ThreadPool> thread_pool()
-    {
-        return details::Registry::instance().tp();
-    }
 } // namespace rexlog
