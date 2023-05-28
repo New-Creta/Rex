359b565bd417e4bdd33194713185255ba8b02c93
Nick De Breuck <nick.debreuck@outlook.com>
2023-05-28 21:21:36 +0100

    [development/invalid_objects] no more use of std. fixing some bugs as well

--- source/1_engine/rex_engine/include/rex_engine/invalid_object.h
+++ source/1_engine/rex_engine/include/rex_engine/invalid_object.h
@@ -11,10 +11,8 @@ namespace rex
   {
     constexpr s32 invalid_value_marker = 0xDEAD; // == 57005
     constexpr size_t object_size       = align(sizeof(T), alignof(int));
-    rsl::array<char, object_size> buff;
-    // to avoid weird issues, intstead of using fill member func
-    // we use memset, as we know that the buffer will have n block of 4 bytes
-    std::memset(buff.data(), invalid_value_marker, object_size / sizeof(invalid_value_marker));
+    rsl::array<int, object_size / sizeof(invalid_value_marker)> buff;
+    buff.fill(invalid_value_marker);
     return *reinterpret_cast<T*>(buff.data());
   }
 } // namespace rex
