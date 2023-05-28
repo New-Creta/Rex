f9864b48de5b2420d315bdbd9379f8f9acf8c5d0
Dave De Breuck <dave.debreuck@twikit.com>
2023-05-28 22:45:16 +0200

    [development/diagnostics_remove_inc_files] Fix merge conflicts

--- source/1_engine/rex_engine/include/rex_engine/diagnostics/logging/internal/details/log_msg.h
+++ source/1_engine/rex_engine/include/rex_engine/diagnostics/logging/internal/details/log_msg.h
@@ -22,36 +22,36 @@ namespace rexlog
             LogMsg(const rsl::source_location& loc, string_view_t loggerName, level::LevelEnum lvl, string_view_t msg);
             LogMsg(string_view_t loggerName, level::LevelEnum lvl, string_view_t msg);
 
-            void                    set_logger_name(string_view_t loggerName);
-            void                    set_level(level::LevelEnum level);
-            void                    set_time(log_clock::time_point tp);
-            void                    set_thread_id(size_t id);
+            void                        set_logger_name(string_view_t loggerName);
+            void                        set_level(level::LevelEnum level);
+            void                        set_time(log_clock::time_point tp);
+            void                        set_thread_id(size_t id);
 
-            void                    set_color_range_start(size_t cs);
-            void                    set_color_range_end(size_t ce);
+            void                        set_color_range_start(size_t cs);
+            void                        set_color_range_end(size_t ce);
 
-            void                    set_source(const rsl::source_location& sourceLoc);
-            void                    set_payload(string_view_t p);
+            void                        set_source(const rsl::source_location& sourceLoc);
+            void                        set_payload(string_view_t p);
 
-            string_view_t           logger_name() const;
-            level::LevelEnum        level() const;
-            log_clock::time_point   time() const;
-            size_t                  thread_id() const;
+            string_view_t               logger_name() const;
+            level::LevelEnum            level() const;
+            log_clock::time_point       time() const;
+            size_t                      thread_id() const;
 
-            size_t                  color_range_start() const;
-            size_t                  color_range_end() const;
+            size_t                      color_range_start() const;
+            size_t                      color_range_end() const;
 
-            SourceLoc               source() const;
-            string_view_t           payload() const;
+            const rsl::source_location& source() const;
+            string_view_t               payload() const;
 
         private:
-            string_view_t           m_logger_name;
-            level::LevelEnum        m_level;
-            log_clock::time_point   m_time;
-            size_t                  m_thread_id;
+            string_view_t               m_logger_name;
+            level::LevelEnum            m_level;
+            log_clock::time_point       m_time;
+            size_t                      m_thread_id;
 
-            rsl::source_location    m_source;
-            string_view_t           m_payload;
+            rsl::source_location        m_source;
+            string_view_t               m_payload;
         };
     } // namespace details
 } // namespace rexlog
