f68d8535f95bf9bf1e6da18833beee3c9d403fb5
Nick De Breuck <nick.debreuck@outlook.com>
2023-06-01 13:56:34 +0100

    [development/command_line_ags] added some extra checks to cmdline parsing

--- source/1_engine/rex_engine/src/cmdline.cpp
+++ source/1_engine/rex_engine/src/cmdline.cpp
@@ -38,6 +38,10 @@ namespace rex
       {
         for(ActiveArgument active_arg: m_arguments)
         {
+          // early optimization that strincmp can't do
+          if (arg.length() != active_arg.argument.length())
+            continue;
+
           if(rsl::strincmp(arg.data(), active_arg.argument.data(), arg.length()) == 0)
           {
             return active_arg.value;
@@ -101,11 +105,27 @@ namespace rex
           value = "1"; // this is so we can easily convert to bool/int
         }
 
-        auto cmd_it = rsl::find_if(g_command_line_args.cbegin(), g_command_line_args.cend(), [key](const Argument& cmdArg) { return key == cmdArg.name; });
+        auto cmd_it = rsl::find_if(g_command_line_args.cbegin(), g_command_line_args.cend(), 
+          [key](const Argument& cmdArg) 
+          { 
+            return key == cmdArg.name; 
+          });
 
         if(cmd_it == g_command_line_args.cend())
         {
           REX_WARN(LogEngine, "Command {} passed in but it's not recognised as a valid command so will be ignored", arg);
+          return;
+        }
+
+        auto active_it = rsl::find_if(m_arguments.cbegin(), m_arguments.cend(),
+        [key](const ActiveArgument& active_arg)
+        {
+          return active_arg.argument == key;
+        });
+
+        if (active_it != m_arguments.cend())
+        {
+          REX_WARN(LogEngine, "Command {} was already passed in. passing the same argument multiple times is not supported. will be skipped", key);
           return;
         }
 
