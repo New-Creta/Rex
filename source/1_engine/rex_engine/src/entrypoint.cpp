fcd9331d49eae9cc2706f2bbb80c7c2819d03757
Nick De Breuck <nick.debreuck@outlook.com>
2023-06-03 02:07:18 +0100

    [development/command_line_ags] added an argument so you can break on boot, this is useful for debugging applications launched without a debugger

--- source/1_engine/rex_engine/src/entrypoint.cpp
+++ source/1_engine/rex_engine/src/entrypoint.cpp
@@ -4,8 +4,10 @@
 #include "rex_engine/diagnostics/logging/logger_config.h"
 #include "rex_engine/filesystem/vfs.h"
 #include "rex_std/array.h"
+#include "rex_std/chrono.h"
 #include "rex_std/internal/exception/exit.h"
 #include "rex_std/string.h"
+#include "rex_std/thread.h"
 
 namespace rex
 {
@@ -20,6 +22,24 @@ namespace rex
       if(cmdline::get_argument("help"))
       {
         cmdline::print_args();
+        rsl::exit(0);
+      }
+
+      // if we want to debug executables without a debugger, we need to break early
+      // so we can attach a debugger and continue from then on
+      // we'll have a timer in place to break for 2 minutes, if no debugger is attached
+      // we close down the program
+      if (cmdline::get_argument("BreakOnBoot"))
+      {
+        using namespace rsl::chrono_literals;
+        auto i = 1s;
+        while (i < 10min)
+        {
+          rsl::this_thread::sleep_for(1s);
+          ++i;
+        }
+
+        // when the debugger is attached, skip this line
         rsl::exit(0);
       }
 
