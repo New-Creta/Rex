876424ce5ffca1cddcc1afef043c8b55981a2c4e
Dave De Breuck <dave.debreuck@twikit.com>
2023-06-06 06:37:44 +0200

    [development/rex_verify] Verify marco

---
+++ source/1_engine/rex_engine/include/rex_engine/diagnostics/verify.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "rex_engine/diagnostics/assert.h"
+
+namespace rex
+{
+  void rex_verify(const rsl::fmt_stack_string& msg);
+}
+
+#define REX_VERIFY(...)                                                                                                                                                                                                                                \
+  rex::rex_verify(rsl::format(__VA_ARGS__));                                                                                                                                                                                                           \
+  while(true)                                                                                                                                                                                                                                          \
+  {                                                                                                                                                                                                                                                    \
+    DEBUG_BREAK();                                                                                                                                                                                                                                     \
+  }
+#define REX_VERIFY_X(cond, ...)                                                                                                                                                                                                                        \
+  if(!(cond))                                                                                                                                                                                                                                          \
+  {                                                                                                                                                                                                                                                    \
+    REX_VERIFY(__VA_ARGS__);                                                                                                                                                                                                                           \
+  }
