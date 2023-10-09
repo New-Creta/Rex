0cf916f10b33b56ef6dbe4d2f4bd7d9ce87bf0c9
Nick De Breuck <nick.debreuck@outlook.com>
2023-10-09 23:53:01 +0100

    [development/improving_docs] the user is now able to specify which test they want to generate. the test input is mapped to the sharpmake arguments needed to support these tests. this simplifies the script.

--- _build/scripts/generate.py
+++ _build/scripts/generate.py
@@ -13,50 +13,45 @@ import argparse
 import regis.generation
 import regis.rex_json
 import regis.util
+import regis.diagnostics
 import glob
 from pathlib import Path
 
+supported_tests = {
+  "unit_test" : " /generateUnitTests",
+  "coverage" : " /generateUnitTests /enableCoverage",
+  "asan" : " /generateUnitTests /enableAddressSanitizer",
+  "ubsan" : " /generateUnitTests /enableUBSanitizer",
+  "fuzzy" : " /enableFuzzyTesting",
+}
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-  parser.add_argument("-enabled_test", default='', help="Enable the solution to run these kind of tests")
+  parser.add_argument("-enabled_test", default='', help=f"Specify the test to be enabled. ({list(supported_tests.keys())}) ")
   parser.add_argument("-no_clang_tools", help="don't generate a compiler db", action="store_true")
-  parser.add_argument("-unit_tests", help="generate unit tests", action="store_true")
-  parser.add_argument("-enable_second_pass", help="Enable second pass of clang-tidy checks", action="store_true")
-  parser.add_argument("-coverage", help="generate coverage", action="store_true")
-  parser.add_argument("-asan", help="generate address sanitizer", action="store_true")
-  parser.add_argument("-ubsan", help="generate undefined behavior sanitizer", action="store_true")
-  parser.add_argument("-fuzzy", help="generate fuzzy testing", action="store_true")
-  parser.add_argument("-sharpmake_args", help="arguments passed to sharpmake")
+  parser.add_argument("-all_clangtidy_checks", help="Perform all configured clang tidy checks. not just the auto fixable ones", action="store_true")
+  parser.add_argument("-sharpmake_args", help="Specify additional arguments to be passed to sharpmake")
 
   args, unknown = parser.parse_known_args()
 
   root = regis.util.find_root()
   settings_path = os.path.join(root, "_build", "config", "settings.json")
 
-  run_any_tests = args.enabled_test
-  run_any_tests = args.unit_tests or args.coverage or args.asan or args.ubsan or args.fuzzy
-
   sharpmake_args = "/enableVisualStudio /disableClangTidyForThirdParty"
+
+  if args.enabled_test:
+    if args.enabled_test in supported_tests:
+      sharpmake_args += supported_tests[args.enabled_test]
+    else:
+      regis.diagnostics.log_err(f'"{args.enabled_test}" not found in supported test')
+      regis.diagnostics.log_err(f'the supported tests are: {supported_tests.keys()}')
+      exit(1)
+  
   if args.no_clang_tools:
     sharpmake_args += " /noClangTools"
 
-  if args.enable_second_pass:
-    sharpmake_args += " /enableSecondPass"
-
-  if "unit_tests" == args.enabled_test:
-    sharpmake_args += " /generateUnitTests"
-
-  elif "coverage" == args.enabled_test:
-    sharpmake_args += " /generateUnitTests /enableCoverage"
-
-  elif "asan" == args.enabled_test:
-    sharpmake_args += " /generateUnitTests /enableAddressSanitizer"
-
-  elif "ubsan" == args.enabled_test:
-    sharpmake_args += " /generateUnitTests /enableUBSanitizer"
-
-  elif "fuzzy" == args.enabled_test:
-    sharpmake_args += " /enableFuzzyTesting"
+  if args.all_clangtidy_checks:
+    sharpmake_args += " /PerformAllClangTidyChecks"
 
   if args.sharpmake_args:
     sharpmake_args += f" {args.sharpmake_args}"
