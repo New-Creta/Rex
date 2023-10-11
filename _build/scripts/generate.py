eab04d57df19d43e0cdeafe8f2cc9d75453ce917
Nick De Breuck <nick.debreuck@outlook.com>
2023-10-12 00:18:16 +0100

    [development/improving_docs] using a config file now to pass arguments between python and sharpmake. This gives us the option to add arguments to the config file and have both sharpmake and python be aware of them (avoid duplicating them in both). the config file is parsed in the generate step, added as commandline arguments to the script (and therefore exposed to the user). a new config file is generated and passed to sharpmake.
    Sharpmake doesn't read this config file yes, that's the next step that needs to get implemented

--- _build/scripts/generate.py
+++ _build/scripts/generate.py
@@ -10,6 +10,7 @@
 
 import os
 import argparse
+import copy
 import regis.generation
 import regis.rex_json
 import regis.util
@@ -17,62 +18,64 @@ import regis.diagnostics
 import glob
 from pathlib import Path
 
-# TODO: Somehow make it happen that these arguments are like plugins
-# that way the maintenance of them is taken out of sharpmake so any script
-# or application can talk to them and these arguments don't need to be just known
-# Avoiding duplication and making it more scalable.
+def make_optional_arg(arg : str):
+  return f'-{arg}'
 
-supported_tests = {
-  "unit_test" : " /generateUnitTests",
-  "coverage" : " /generateUnitTests /enableCoverage",
-  "asan" : " /generateUnitTests /enableAddressSanitizer",
-  "ubsan" : " /generateUnitTests /enableUBSanitizer",
-  "fuzzy" : " /enableFuzzyTesting",
-}
+def _add_config_arguments(parser : argparse.ArgumentParser, defaultConfig : dict):
+  for setting in defaultConfig:
+    arg = make_optional_arg(setting)
+    val = default_config[setting]['value']
+    desc = default_config[setting]['description']
+    if 'options' in default_config[setting]:
+      desc += f' Options: {default_config[setting]["options"]}'
+    
+    if type(val) == bool:
+      parser.add_argument(arg, help=desc, action='store_true')
+    else:
+      parser.add_argument(arg, help=desc, default=val)
+
+def _create_new_config(defaultConfig : dict, args):
+  config : dict = copy.deepcopy(default_config)
+  for arg in vars(args):
+    arg_name = arg
+    arg_name = arg_name.replace('_', '-') # python converts all hyphens into underscores so we need to convert them back
+    arg_val = getattr(args, arg)
+
+    if arg_name in config:
+      config[arg_name]['value'] = arg_val
+
+  return config
+
+def _save_config_file(settings : dict, config : dict):
+  config_dir = os.path.join(os.path.join(root, settings['intermediate_folder'], settings['build_folder']))
+  if not os.path.exists(config_dir):
+    os.mkdir(config_dir)
+
+  config_path = os.path.join(config_dir, 'config.json')
+  regis.rex_json.save_file(config_path, config)
+
+  return config_path
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
-  parser.add_argument("-enabled_test", default='', help=f"Specify the test to be enabled. ({list(supported_tests.keys())}) ")
-  parser.add_argument("-no_clang_tools", help="don't generate a compiler db", action="store_true")
-  parser.add_argument("-all_clangtidy_checks", help="Perform all configured clang tidy checks. not just the auto fixable ones", action="store_true")
-  parser.add_argument("-sharpmake_args", help="Specify additional arguments to be passed to sharpmake")
-
-  args, unknown = parser.parse_known_args()
-
-  sharpmake_args = "/enableVisualStudio /disableClangTidyForThirdParty"
-
-  # If a test is specified, look up its arguments and add them
-  # to the arguments we'll pass to sharpmake
-  if args.enabled_test:
-    if args.enabled_test in supported_tests:
-      sharpmake_args += supported_tests[args.enabled_test]
-    else:
-      regis.diagnostics.log_err(f'"{args.enabled_test}" not found in supported test')
-      regis.diagnostics.log_err(f'the supported tests are: {supported_tests.keys()}')
-      exit(1)
-  
-  if args.no_clang_tools:
-    sharpmake_args += " /noClangTools"
-
-  if args.all_clangtidy_checks:
-    sharpmake_args += " /PerformAllClangTidyChecks"
-
-  if args.sharpmake_args:
-    sharpmake_args += f" {args.sharpmake_args}"
-
-  # Apparently we cannot pass double quotes as an argument to python
-  # At least by my knowledge ... 
-  sharpmake_args = sharpmake_args.replace("\'", "\"")
-  # We also need to make sure we use forward slashes.
-  sharpmake_args = sharpmake_args.replace("\\", "\/")
-
   root = regis.util.find_root()
-  settings_path = os.path.join(root, "_build", "config", "settings.json")
+  default_config : dict = regis.rex_json.load_file(os.path.join(root, "_build", "sharpmake", "data", "default_config.json"))
 
-  # To avoid duplicating code in the toolchain and to avoid hardcoded paths
-  # as much as possible, we save most paths, extensions and other hardcoded values
-  # into a settings file.
-  # this way multiple script can use these settings, including the engine itself.
+  # initialize the argument parser by loading the arguments from the config file
+  parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+  parser.add_argument('-clean', help='Clean the intermediates before generation')
+  _add_config_arguments(parser, default_config)
+
+  # parse the arguments passed to the script
+  args, unknown = parser.parse_known_args()
+  config = _create_new_config(default_config, args)
+
+  # save the config file to disk
+  settings_path = os.path.join(root, "_build", "config", "settings.json")
+  settings = regis.rex_json.load_file(settings_path)
+  config_path = _save_config_file(settings, config)
+
+  # call generation code to launch sharpmake
+  sharpmake_args = f"/configFile({config_path})"
   proc = regis.generation.new_generation(settings_path, sharpmake_args)
   proc.wait()
   result = proc.returncode
