362c10ea6d469ddb460464a383b6cbdc98b9af5b
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2023-10-18 08:34:45 +0200

    [development/directx_init_drawing] Link dx libraries

--- source/2_platform/rex_directx/include/rex_directx/directx_util.h
+++ source/2_platform/rex_directx/include/rex_directx/directx_util.h
@@ -8,6 +8,11 @@
 #include <d3d12.h>
 #include <d3d11on12.h>
 
+// Link necessary d3d12 libraries.
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
 namespace rex
 {
     namespace renderer
