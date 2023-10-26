8fb898fb765dac34864b7f52c59ace231804ec81
Dave De Breuck <dave.debreuck@twikit.com>
2023-10-26 19:24:44 +0200

    [development/directx_init_drawing] Renderer utility types

---
+++ source/1_engine/rex_renderer_core/include/rex_renderer_core/scissor_rect.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "rex_engine/types.h"
+
+namespace rex
+{
+    struct ScissorRect
+    {
+        f32 left;
+        f32 top;
+        f32 right;
+        f32 bottom;
+    };
+} // namespace rex
--- source/1_engine/rex_renderer_core/include/rex_renderer_core/viewport.h
+++ source/1_engine/rex_renderer_core/include/rex_renderer_core/viewport.h
@@ -4,7 +4,7 @@
 
 namespace rex
 {
-  struct ViewportCreationInfo
+  struct Viewport
   {
     f32 top_left_x;
     f32 top_left_y;
@@ -14,29 +14,5 @@ namespace rex
 
     f32 min_depth;
     f32 max_depth;
-  };
-
-  class Viewport
-  {
-  public:
-    Viewport();
-    explicit Viewport(const ViewportCreationInfo& info);
-
-    f32 top_left_x() const;
-    f32 top_left_y() const;
-    f32 width() const;
-    f32 height() const;
-    f32 min_depth() const;
-    f32 max_depth() const;
-
-  private:
-    f32 m_top_left_x;
-    f32 m_top_left_y;
-
-    f32 m_width;
-    f32 m_height;
-
-    f32 m_min_depth;
-    f32 m_max_depth;
   };
 } // namespace rex
