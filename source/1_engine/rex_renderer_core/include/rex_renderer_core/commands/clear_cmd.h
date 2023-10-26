5232f23284e622c067148f9bd0a18708b0d51407
Dave De Breuck <dave.debreuck@twikit.com>
2023-10-26 23:51:26 +0200

    [development/directx_init_drawing] Array index and texture index in clear cmd

--- source/1_engine/rex_renderer_core/include/rex_renderer_core/commands/clear_cmd.h
+++ source/1_engine/rex_renderer_core/include/rex_renderer_core/commands/clear_cmd.h
@@ -12,9 +12,13 @@ namespace rex
             {
                 Clear()
                     :clear_state(REX_INVALID_INDEX)
+                    ,array_index(REX_INVALID_INDEX)
+                    ,texture_index(REX_INVALID_INDEX)
                 {}
 
                 u32 clear_state;
+                u32 array_index;
+                u32 texture_index;
             };
         }
     }
