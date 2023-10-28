aebe2582f807f042a2c9f954086c13b84d3030e8
Nick De Breuck <nick.debreuck@outlook.com>
2023-10-29 00:17:35 +0100

    [development/improving_docs] adding build projects json file after a build finished. this works with the file watcher when a build happens, we store which exes paths got generated

--- _build/scripts/build.py
+++ _build/scripts/build.py
@@ -8,6 +8,9 @@
 #
 # ============================================
 
+# This script acts as the interface to build the rex engine and its tools
+# it supports setting the compiler, configuration as well as the project to build.
+
 import os
 import sys
 import argparse
@@ -15,9 +18,53 @@ import regis.build
 import regis.util
 import regis.diagnostics
 import regis.task_raii_printing
+import regis.dir_watcher
+import regis.rex_json
+
+root = regis.util.find_root()  
+settings_path = os.path.join(root, "_build", "config", "settings.json")
+settings = regis.rex_json.load_file(settings_path)
+intermediate_path = os.path.join(root, settings['intermediate_folder'], settings['build_folder'])
+build_projects_path = os.path.join(intermediate_path, settings['build_projects_filename'])
+
+def update_cleaned_projects(project : str, config : str, compiler : str, deletedPrograms : list[str]):
+  build_projects = regis.rex_json.load_file(build_projects_path)
+
+  if project not in build_projects:
+    return
+  
+  build_project = build_projects[project]
+  if config not in build_project:
+    return
+  
+  build_config = build_projects[project][config]
+  if compiler not in build_config:
+    return
+  
+  build_programs : list[str] = build_projects[project][config][compiler]
+
+  for deleted_program in deletedPrograms:
+    if deleted_program in build_programs:
+      build_programs.remove(deleted_program)
+
+  regis.rex_json.save_file(build_projects_path, build_projects)
+
+def update_build_projects(project : str, config : str, compiler : str, paths : list[str]):
+  build_projects = regis.rex_json.load_file(build_projects_path)
+
+  if project not in build_projects:
+    build_projects[project] = {}
+  
+  build_project = build_projects[project]
+  if config not in build_project:
+    build_project[config] = {}
+    
+  build_projects[project][config][compiler] = paths
+
+  regis.rex_json.save_file(build_projects_path, build_projects)
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
+  parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   
   # There a couple ways of calling this script, but in essence it's very simple
   # build.py                 <-- builds the project
@@ -39,8 +86,18 @@ if __name__ == "__main__":
     else:
       args.compiler = "clang"
 
-  task = regis.task_raii_printing.TaskRaiiPrint("Building")
-  result = regis.build.new_build(args.project, args.config, args.compiler, not args.nobuild, args.clean, args.sln, not args.dont_build_dependencies)
+  with regis.dir_watcher.DirWatcher(intermediate_path, bRecursive=True) as dir_watcher:
+    task = regis.task_raii_printing.TaskRaiiPrint("Building")
+    result = regis.build.new_build(args.project, args.config, args.compiler, not args.nobuild, args.clean, args.sln, not args.dont_build_dependencies)
+
+  if not os.path.exists(build_projects_path):
+    regis.rex_json.save_file(build_projects_path, {})
+
+  programs_deleted = dir_watcher.filter_deleted_files(lambda path: path.endswith('.exe'))
+  programs_created = dir_watcher.filter_created_or_modified_files(lambda path: path.endswith('.exe'))
+
+  update_cleaned_projects(args.project, args.config, args.compiler, programs_deleted)
+  update_build_projects(args.project, args.config, args.compiler, programs_created)
 
   if result != 0:
     regis.diagnostics.log_err("Build failed")
