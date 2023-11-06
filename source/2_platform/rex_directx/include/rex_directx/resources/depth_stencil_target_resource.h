53b7afc5d0057beb3a3144539f0b6c56d3c9e9e9
Dyronix <davedebreuck.gamedeveloper@gmail.com>
2023-11-06 21:13:42 +0100

    [development/directx_init_drawing] render target resource & depth stencil target resource

---
+++ source/2_platform/rex_directx/include/rex_directx/resources/depth_stencil_target_resource.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "rex_directx/directx_util.h"
+#include "rex_directx/resources/resource.h"
+#include "rex_engine/types.h"
+
+namespace rex
+{
+  namespace renderer
+  {
+    namespace resources
+    {
+      struct DepthStencilTarget
+      {
+        wrl::com_ptr<ID3D12Resource> render_target;
+        s32 array_index;
+      };
+    } // namespace resources
+
+    class DepthStencilTargetResource : public BaseResource<resources::DepthStencilTarget>
+    {
+    public:
+      DepthStencilTargetResource(const wrl::com_ptr<ID3D12Resource>& dst, s32 arrayIndex)
+          : m_depth_stencil_target({dst, arrayIndex})
+      {
+      }
+      ~DepthStencilTargetResource() override = default;
+
+      resources::DepthStencilTarget* get()
+      {
+        return &m_depth_stencil_target;
+      }
+      const resources::DepthStencilTarget* get() const
+      {
+        return &m_depth_stencil_target;
+      }
+
+    private:
+      resources::DepthStencilTarget m_depth_stencil_target;
+    };
+  } // namespace renderer
+} // namespace rex
--- source/2_platform/rex_directx/include/rex_directx/resources/render_target_resource.h
+++ source/2_platform/rex_directx/include/rex_directx/resources/render_target_resource.h
@@ -1,9 +1,42 @@
 #pragma once
 
+#include "rex_directx/directx_util.h"
+#include "rex_directx/resources/resource.h"
+#include "rex_engine/types.h"
+
 namespace rex
 {
-    namespace renderer
+  namespace renderer
+  {
+    namespace resources
     {
+      struct RenderTarget
+      {
+        wrl::com_ptr<ID3D12Resource> render_target;
+        s32 array_index;
+      };
+    } // namespace resources
 
-    }
-}
+    class RenderTargetResource : public BaseResource<resources::RenderTarget>
+    {
+    public:
+      RenderTargetResource(const wrl::com_ptr<ID3D12Resource>& rt, s32 arrayIndex)
+          : m_render_target({rt, arrayIndex})
+      {
+      }
+      ~RenderTargetResource() override = default;
+
+      resources::RenderTarget* get()
+      {
+        return &m_render_target;
+      }
+      const resources::RenderTarget* get() const
+      {
+        return &m_render_target;
+      }
+
+    private:
+      resources::RenderTarget m_render_target;
+    };
+  } // namespace renderer
+} // namespace rex
