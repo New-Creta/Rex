006b4853c45cc734141b33ba22757c3102da3f33
Dave De Breuck <dave.debreuck@twikit.com>
2023-11-06 23:20:04 +0100

    [development/directx_init_drawing] Remove unused file Boyscout

--- source/1_engine/rex_renderer_core/include/rex_renderer_core/viewport.h
+++ source/1_engine/rex_renderer_core/include/rex_renderer_core/viewport.h
@@ -4,15 +4,15 @@
 
 namespace rex
 {
-  struct Viewport
-  {
-    f32 top_left_x;
-    f32 top_left_y;
+    struct Viewport
+    {
+        f32 top_left_x;
+        f32 top_left_y;
 
-    f32 width;
-    f32 height;
+        f32 width;
+        f32 height;
 
-    f32 min_depth;
-    f32 max_depth;
-  };
+        f32 min_depth;
+        f32 max_depth;
+    };
 } // namespace rex
