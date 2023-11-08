5addfbf86d9eab17c4c073e659a3dbe14180f622
Dave De Breuck <dave.debreuck@twikit.com>
2023-11-08 20:16:42 +0100

    [development/directx_init_drawing] store the constants of a shader program in the shader program resource

--- source/2_platform/rex_directx/include/rex_directx/resources/shader_program_resource.h
+++ source/2_platform/rex_directx/include/rex_directx/resources/shader_program_resource.h
@@ -4,6 +4,8 @@
 #include "rex_directx/directx_util.h"
 #include "rex_directx/wrl/wrl_types.h"
 
+#include "rex_renderer_core/parameters/constant_layout_description_params.h"
+
 namespace rex
 {
     namespace renderer
@@ -15,14 +17,17 @@ namespace rex
                 wrl::com_ptr<ID3D12RootSignature> root_signature;
                 wrl::com_ptr<ID3DBlob> vertex_shader;
                 wrl::com_ptr<ID3DBlob> pixel_shader;
+
+                parameters::ConstantLayoutDescription* constants;
+                s32 num_constants;
             };
         }
 
         class ShaderProgramResource : public BaseResource<resources::ShaderProgram>
         {
         public:
-            ShaderProgramResource(const wrl::com_ptr<ID3D12RootSignature>& rootSig, const wrl::com_ptr<ID3DBlob>& vs, const wrl::com_ptr<ID3DBlob>& ps)
-                :m_shader_program({ rootSig, vs, ps })
+            ShaderProgramResource(const wrl::com_ptr<ID3D12RootSignature>& rootSig, const wrl::com_ptr<ID3DBlob>& vs, const wrl::com_ptr<ID3DBlob>& ps, parameters::ConstantLayoutDescription* constants, s32 numConstants)
+                :m_shader_program({ rootSig, vs, ps, constants, numConstants })
             {}
             ~ShaderProgramResource() override = default;
 
