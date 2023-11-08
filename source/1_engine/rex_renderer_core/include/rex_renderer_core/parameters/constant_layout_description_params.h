d413c673f3f1f1e7c7cbd9d05e6074d1c7c4f1bf
Dave De Breuck <dave.debreuck@twikit.com>
2023-11-08 20:13:25 +0100

    [development/directx_init_drawing] constant layout parameters

---
+++ source/1_engine/rex_renderer_core/include/rex_renderer_core/parameters/constant_layout_description_params.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "rex_engine/types.h"
+
+namespace rex
+{
+    namespace renderer
+    {
+        namespace parameters
+        {
+            enum class ConstantType
+            {
+                SAMPLER_2D = 0,
+                SAMPLER_3D,
+                SAMPLER_CUBE,
+                SAMPLER_2DMS,
+                SAMPLER_2D_ARRAY,
+                SAMPLER_2D_DEPTH,
+                SAMPLER_2D_DEPTH_ARRAY,
+                CBUFFER,
+                CONSTANT
+            };
+
+            struct ConstantLayoutDescription
+            {
+                ConstantType type;
+
+                char8* name;
+                s32 location;
+            };
+        }
+    }
+}
