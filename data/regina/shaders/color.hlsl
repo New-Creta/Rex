be51bc80c8ffe1f236fe38bb3b20e4b925762def
Dave De Breuck <dave.debreuck@twikit.com>
2023-11-26 14:43:29 +0100

    [development/directx_init_drawing] Update color shader

--- data/regina/shaders/color.hlsl
+++ data/regina/shaders/color.hlsl
@@ -3,35 +3,51 @@
 //
 // Transforms and colors geometry.
 //***************************************************************************************
-
+ 
 cbuffer cbPerObject : register(b0)
 {
-	float4x4 gWorldViewProj; 
+    float4x4 gWorld;
+};
+
+cbuffer cbPass : register(b1)
+{
+    float4x4 gView;
+    float4x4 gInvView;
+    float4x4 gProj;
+    float4x4 gInvProj;
+    float4x4 gViewProj;
+    float4x4 gInvViewProj;
+    float3 gEyePosW;
+    float cbPerObjectPad1;
+    float2 gRenderTargetSize;
+    float2 gInvRenderTargetSize;
+    float gNearZ;
+    float gFarZ;
+    float gDeltaTime;
 };
 
 struct VertexIn
 {
-	float3 PosL  : POSITION;
+    float3 PosL : POSITION;
     float4 Color : COLOR;
 };
 
 struct VertexOut
 {
-	float4 PosH  : SV_POSITION;
+    float4 PosH : SV_POSITION;
     float4 Color : COLOR;
 };
 
 VertexOut VS(VertexIn vin)
 {
-	VertexOut vout;
+    VertexOut vout;
 	
 	// Transform to homogeneous clip space.
-	vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosH = mul(posW, gViewProj);
 	
 	// Just pass vertex color into the pixel shader.
-    float3 sRGB = saturate(vin.Color.rgb);
-    sRGB = pow(sRGB, 1.0 / 2.2);
-    vout.Color = float4(sRGB, 1.0);
+    vout.Color = vin.Color;
     
     return vout;
 }
