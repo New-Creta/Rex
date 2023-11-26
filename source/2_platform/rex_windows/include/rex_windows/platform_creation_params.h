d85b0b1e0d12971e80ed506f8de1be62c8049bbb
Dave De Breuck <dave.debreuck@twikit.com>
2023-11-26 14:40:40 +0100

    [development/directx_init_drawing] Be able to specify the amount of frames in flight

--- source/2_platform/rex_windows/include/rex_windows/platform_creation_params.h
+++ source/2_platform/rex_windows/include/rex_windows/platform_creation_params.h
@@ -24,6 +24,7 @@ namespace rex
     bool fullscreen               = false;
 
     s32 max_render_commands       = 1 << 16;
+    s32 max_frames_in_flight      = 3;
     s32 max_fps                   = 60;
   };
 
