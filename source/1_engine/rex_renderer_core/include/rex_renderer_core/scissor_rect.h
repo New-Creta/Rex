df24f4f7c3135302abdd43c79f1775c4e83443ee
DaveDeBreuck <dave.debreuck@twikit.com>
2023-12-15 11:25:23 +0100

    [development/directx_init_drawing_switch_pc] Initialize viewport and scissorrect objects

--- source/1_engine/rex_renderer_core/include/rex_renderer_core/scissor_rect.h
+++ source/1_engine/rex_renderer_core/include/rex_renderer_core/scissor_rect.h
@@ -6,6 +6,20 @@ namespace rex
 {
     struct ScissorRect
     {
+        ScissorRect()
+            :left(0.0f)
+            ,top(0.0f)
+            ,right(0.0f)
+            ,bottom(0.0f)
+        {}
+
+        ScissorRect(f32 l, f32 t, f32 r, f32 b)
+            :left(l)
+            ,top(t)
+            ,right(r)
+            ,bottom(b)
+        {}
+
         f32 left;
         f32 top;
         f32 right;
--- source/1_engine/rex_renderer_core/include/rex_renderer_core/viewport.h
+++ source/1_engine/rex_renderer_core/include/rex_renderer_core/viewport.h
@@ -6,6 +6,24 @@ namespace rex
 {
     struct Viewport
     {
+        Viewport()
+            :top_left_x(0.0f)
+            ,top_left_y(0.0f)
+            ,width(0.0f)
+            ,height(0.0f)
+            ,min_depth(0.0f)
+            ,max_depth(0.0f)
+        {}
+
+        Viewport(f32 topLeftX, f32 topLeftY, f32 vpWith, f32 vpHeight, f32 minDepth, f32 maxDepth)
+            :top_left_x(topLeftX)
+            , top_left_y(topLeftY)
+            , width(vpWith)
+            , height(vpHeight)
+            , min_depth(minDepth)
+            , max_depth(maxDepth)
+        {}
+
         f32 top_left_x;
         f32 top_left_y;
 
