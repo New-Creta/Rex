ef3d216197daeea3ccea124be32b955ca68594bf
Nick De Breuck <nick.debreuck@outlook.com>
2023-12-24 14:36:08 +0000

    [development/file_reorganisation] reorganisation of files that have platform specific code

--- source/1_engine/rex_engine/include/rex_engine/engine/defines.h
+++ source/1_engine/rex_engine/include/rex_engine/engine/defines.h
@@ -35,4 +35,15 @@
   // BIT TWIDDLING
   #define BIT(x) (1 << x)
 
+
+  //-------------------------------------------------------------------------
+  // Func signature
+  #if defined REX_COMPILER_CLANG
+    #define REX_FUNC_SIG __PRETTY_FUNCTION__
+  #elif defined REX_COMPILER_MSVC
+    #define REX_FUNC_SIG __FUNCSIG__
+  #else
+    #define REX_FUNC_SIG "REX_FUNC_SIG unknown!"
+  #endif
+
 #endif
