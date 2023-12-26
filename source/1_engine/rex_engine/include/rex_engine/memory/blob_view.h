365632aad3e8bd51533b6c11a161664bf39da3f1
Nick De Breuck <nick.debreuck@outlook.com>
2023-12-26 11:27:44 +0000

    [development/memory_view] fixing compiler errors

--- source/1_engine/rex_engine/include/rex_engine/memory/blob_view.h
+++ source/1_engine/rex_engine/include/rex_engine/memory/blob_view.h
@@ -1,31 +1,38 @@
 #pragma once
 
+#include "rex_std/memory.h"
+#include "rex_std/bonus/memory.h"
+
 namespace rex
 {
-  class Blob;
-
-  // Similar design of string_view -> string.
-  // A blob view has a const view over a blob
-  // It doesn't own the blob but can provide an interface into
-  // the data of a blob
-
-  class BlobView
+  namespace memory
   {
-  public:
-    BlobView();
-    BlobView(const Blob& blob);
 
-    const rsl::byte* data() const;
-    rsl::memory_size size() const;
+    class Blob;
 
-    template <typename T>
-    const T* data_as() const
+    // Similar design of string_view -> string.
+    // A blob view has a const view over a blob
+    // It doesn't own the blob but can provide an interface into
+    // the data of a blob
+
+    class BlobView
     {
-      return static_cast<T*>(data());
-    }
+    public:
+      BlobView();
+      BlobView(const Blob& blob);
 
-  private:
-    rsl::byte* m_data;
-    rsl::memory_size m_size;
-  };
+      const rsl::byte* data() const;
+      rsl::memory_size size() const;
+
+      template <typename T>
+      const T* data_as() const
+      {
+        return static_cast<T*>(data());
+      }
+
+    private:
+      const rsl::byte* m_data;
+      rsl::memory_size m_size;
+    };
+  }
 }
--- source/1_engine/rex_engine/src/memory/blob_view.cpp
+++ source/1_engine/rex_engine/src/memory/blob_view.cpp
@@ -1,24 +1,29 @@
 #include "rex_engine/memory/blob_view.h"
 
+#include "rex_engine/memory/blob.h"
+
 namespace rex
 {
-  BlobView::BlobView()
-    : m_data(nullptr)
-    , m_size(0)
-  {}
-
-  BlobView::BlobView(const Blob& blob)
-    : m_data(blob.data())
-    , m_size(blob.size())
-  {}
-
-  const rsl::byte* BlobView::data() const
+  namespace memory
   {
-    return m_data;
-  }
+    BlobView::BlobView()
+      : m_data(nullptr)
+      , m_size(0)
+    {}
 
-  rsl::memory_size BlobView::size() const
-  {
-    return m_size;
+    BlobView::BlobView(const Blob& blob)
+      : m_data(blob.data())
+      , m_size(blob.size())
+    {}
+
+    const rsl::byte* BlobView::data() const
+    {
+      return m_data;
+    }
+
+    rsl::memory_size BlobView::size() const
+    {
+      return m_size;
+    }
   }
 }
