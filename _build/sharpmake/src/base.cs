6c39dcbdd9dce17b01578b8f5735ff1612c3d7a8
Nick De Breuck <nick.debreuck@outlook.com>
2024-02-03 17:23:15 +0000

    [main] moving the config folder per project next to the config sources so it's easier integrated into the IDE, makes it much easier to work with

--- _build/sharpmake/src/base.cs
+++ _build/sharpmake/src/base.cs
@@ -109,9 +109,19 @@ public class RegenerateProjects : Project
     string rexpyPath = Path.Combine(Globals.Root, "_rex.py");
 
     // The custom build steps just perform a generation step
+    string IdeCommandLineOption = "VisualStudio19";
+    switch (ProjectGen.Settings.IDE)
+    {
+      case ProjectGen.IDE.VisualStudio19: IdeCommandLineOption = "VisualStudio19"; break;
+      case ProjectGen.IDE.VisualStudio22: IdeCommandLineOption = "VisualStudio22"; break;
+      case ProjectGen.IDE.VSCode: IdeCommandLineOption = "VSCode"; break;
+      default:
+        break;
+    }
+
     conf.CustomBuildSettings = new Configuration.NMakeBuildSettings();
-    conf.CustomBuildSettings.BuildCommand = $"py {rexpyPath} generate -no_default_config -IDE VisualStudio"; // Use what's previously generated
-    conf.CustomBuildSettings.RebuildCommand = $"py {rexpyPath} generate -IDE VisualStudio"; // Perform a generation from scratch
+    conf.CustomBuildSettings.BuildCommand = $"py {rexpyPath} generate -no_default_config -IDE {IdeCommandLineOption}"; // Use what's previously generated
+    conf.CustomBuildSettings.RebuildCommand = $"py {rexpyPath} generate -IDE {IdeCommandLineOption}"; // Perform a generation from scratch
     conf.CustomBuildSettings.CleanCommand = "";
     conf.CustomBuildSettings.OutputFile = "";
   }
@@ -134,6 +144,8 @@ public abstract class BasicCPPProject : Project
     LoadToolPaths();
 
     ClangToolsEnabled = ProjectGen.Settings.ClangToolsEnabled;
+
+    ResourceFilesExtensions.Add(".json");
   }
 
   // Legacy function and should be removed
