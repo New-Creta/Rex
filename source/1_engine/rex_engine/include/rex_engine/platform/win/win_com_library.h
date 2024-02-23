ed26b7a2a335236d8121242eb04c054803e97ade
Nick De Breuck <nick.debreuck@outlook.com>
2024-02-23 21:08:08 +0000

    [development/changing_win_com_lib_design] redesigned the win com lib api so that users don't have direct access to win com library object

--- source/1_engine/rex_engine/include/rex_engine/platform/win/win_com_library.h
+++ source/1_engine/rex_engine/include/rex_engine/platform/win/win_com_library.h
@@ -5,55 +5,52 @@
 #include "rex_std/bonus/string.h"
 #include "rex_std/bonus/types.h"
 
+
 #include <Windows.h>
 #include <d3d12.h>
 #include <stddef.h>
 
+#include "rex_engine/platform/win/diagnostics/hr_call.h"
+#include "rex_engine/platform/win/win_com_ptr.h"
+
 namespace rex::win
 {
-  class ComLibrary
+  namespace com_lib
   {
-  public:
-    ComLibrary();
-    ~ComLibrary();
+    // A WinComLibHandle makes sure the win com lib gets destroyed
+    // when the handle goes out of scope
+    class WinComLibHandle
+    {
+    public:
+      ~WinComLibHandle();
+    };
 
-    ComLibrary(const ComLibrary&) = delete;
-    ComLibrary(ComLibrary&&)      = delete;
+    // Creates a WinComHandle.
+    // Makes sure the win com lib is initialized before creating the handle
+    WinComLibHandle create_lib_handle();
 
-    ComLibrary& operator=(const ComLibrary&) = delete;
-    ComLibrary& operator=(ComLibrary&&)      = delete;
+    // Return if the win com lib is initialized already or not
+    bool is_initialized();
 
-    bool is_initialized() const;
+    // Read a symbolic link's path and return the path it actually points to
+    rsl::string read_link(rsl::string_view filepath);
 
+    // Create a com object using the com lib
     template <typename ComObject>
     wrl::ComPtr<ComObject> create_com_object()
     {
-      ComPtr<ComObject> com_object;
-      HR_CALL(CoCreateInstance(__uuidof(ComObject), NULL, CLSCTX_ALL, IID_PPV_ARGS(com_object.get_address_of())));
+      wrl::ComPtr<ComObject> com_object;
+      HR_CALL(CoCreateInstance(__uuidof(ComObject), NULL, CLSCTX_ALL, IID_PPV_ARGS(com_object.GetAddressOf())));
       return com_object;
     }
 
+    // Create a com object using the com lib
     template <typename ComObject>
     wrl::ComPtr<ComObject> create_com_object(IID id)
     {
-      ComPtr<ComObject> com_object;
-      HR_CALL(CoCreateInstance(id, NULL, CLSCTX_ALL, IID_PPV_ARGS(com_object.get_address_of())));
+      wrl::ComPtr<ComObject> com_object;
+      HR_CALL(CoCreateInstance(id, NULL, CLSCTX_ALL, IID_PPV_ARGS(com_object.GetAddressOf())));
       return com_object;
     }
-
-    rsl::string read_link(rsl::string_view filepath);
-
-  private:
-    bool init_lib();
-    void uninit_lib();
-
-    void inc_ref();
-    void dec_ref();
-
-  private:
-    static thread_local card32 s_init_succeeded_count;
-  };
-
-  bool init_com_library();
-  ComLibrary& com_library();
+  }
 } // namespace rex::win
