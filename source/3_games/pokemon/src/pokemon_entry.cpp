ea26915ab4a1490629e92bb1d059cb129abe90d2
Nick De Breuck <nick.debreuck@outlook.com>
2024-04-07 22:58:47 +0100

    [pokemon] creating pokemon app rex entry functions

--- source/3_games/pokemon/src/pokemon_entry.cpp
+++ source/3_games/pokemon/src/pokemon_entry.cpp
@@ -1,5 +1,29 @@
 #include "rex_engine/engine/entrypoint.h"
 
+#include "rex_engine/diagnostics/log.h"
+
+namespace pokemon
+{
+  DEFINE_LOG_CATEGORY(LogPokemon);
+
+  bool initialise()
+  {
+    REX_INFO(LogPokemon, "Pokemon initialized");
+
+    return true;
+  }
+
+  void update()
+  {
+
+  }
+
+  void shutdown()
+  {
+
+  }
+}
+
 namespace rex
 {
   rsl::string_view project_name()
@@ -13,9 +37,13 @@ namespace rex
 
     app_params.gui_params.window_width = 1280;
     app_params.gui_params.window_height = 720;
-    app_params.gui_params.window_title = "Pokemon";
+    app_params.gui_params.window_title = "Pokemon: Rex";
 
     app_params.create_window = true;
+
+    app_params.engine_params.app_init_func = pokemon::initialise;
+    app_params.engine_params.app_update_func = pokemon::update;
+    app_params.engine_params.app_shutdown_func = pokemon::shutdown;
 
     return app_params;
   }
