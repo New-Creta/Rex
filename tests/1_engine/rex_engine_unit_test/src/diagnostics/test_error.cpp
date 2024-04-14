5d72315d3775cf09f9aa8263631c3a1ecc285ec5
Nick De Breuck <nick.debreuck@outlook.com>
2024-04-14 19:00:58 +0100

    [nick_main] adding tests for error, numeric and state controller

--- tests/1_engine/rex_engine_unit_test/src/diagnostics/test_error.cpp
+++ tests/1_engine/rex_engine_unit_test/src/diagnostics/test_error.cpp
@@ -2,7 +2,22 @@
 
 #include "rex_engine/diagnostics/error.h"
 
-TEST_CASE("")
+TEST_CASE("Error without error")
 {
+  rex::Error err = rex::Error::no_error();
 
+  REX_CHECK(err.error_code() == 0);
+  REX_CHECK(err.has_error() == false);
+  REX_CHECK(err.operator bool() == false);
+  REX_CHECK(err.error_msg() == "");
+}
+
+TEST_CASE("Error with error")
+{
+  rex::Error err = rex::Error("some error msg");
+
+  REX_CHECK(err.error_code() != 0);
+  REX_CHECK(err.has_error() == true);
+  REX_CHECK(err.operator bool() == true);
+  REX_CHECK(err.error_msg() == "some error msg");
 }
--- tests/1_engine/rex_unit_test/include/rex_unit_test/test_object.h
+++ tests/1_engine/rex_unit_test/include/rex_unit_test/test_object.h
@@ -4,49 +4,46 @@
 
 namespace rex::test
 {
-  inline namespace v1
+  class test_object
   {
-    class test_object
-    {
-    public:
-      test_object(card32 x = 0);
-      test_object(card32 x, card32 y, card32 z);
-      test_object(const test_object& obj);
-      test_object(test_object&& obj);
-      ~test_object();
+  public:
+    test_object(card32 x = 0);
+    test_object(card32 x, card32 y, card32 z);
+    test_object(const test_object& obj);
+    test_object(test_object&& obj);
+    ~test_object();
 
-      test_object& operator=(const test_object& obj);
-      test_object& operator=(test_object&& obj);
+    test_object& operator=(const test_object& obj);
+    test_object& operator=(test_object&& obj);
 
-      card32 x() const;
-      operator card32() const; // this is to easily compare with integers
+    card32 x() const;
+    operator card32() const; // this is to easily compare with integers
 
-      static bool is_clear();
-      static void reset();
-      static card32 num_created();
-      static card32 num_ctor_calls();
-      static card32 num_dtor_calls();
-      static card32 num_copy_ctor_calls();
-      static card32 num_move_ctor_calls();
-      static card32 num_copy_assignment_calls();
-      static card32 num_move_assignment_calls();
+    static bool is_clear();
+    static void reset();
+    static card32 num_created();
+    static card32 num_ctor_calls();
+    static card32 num_dtor_calls();
+    static card32 num_copy_ctor_calls();
+    static card32 num_move_ctor_calls();
+    static card32 num_copy_assignment_calls();
+    static card32 num_move_assignment_calls();
 
-    private:
-      card32 m_x; // value for this instance
-      static card32 s_num_created; // number of created objects
-      static card32 s_num_ctor_calls; // number of constructor calls
-      static card32 s_num_dtor_calls; // number of constructor calls
-      static card32 s_num_copy_ctor_calls; // number of copy constructor calls
-      static card32 s_num_move_ctor_calls; // number of move constructor calls
-      static card32 s_num_copy_assignment_calls; // number of copy assignment calls
-      static card32 s_num_move_assignment_calls; // number of move assignment calls
-    };
+  private:
+    card32 m_x; // value for this instance
+    static card32 s_num_created; // number of created objects
+    static card32 s_num_ctor_calls; // number of constructor calls
+    static card32 s_num_dtor_calls; // number of constructor calls
+    static card32 s_num_copy_ctor_calls; // number of copy constructor calls
+    static card32 s_num_move_ctor_calls; // number of move constructor calls
+    static card32 s_num_copy_assignment_calls; // number of copy assignment calls
+    static card32 s_num_move_assignment_calls; // number of move assignment calls
+  };
 
-    bool operator==(const test_object& lhs, const test_object& rhs);
-    bool operator==(const test_object& lhs, card32 rhs);
-    bool operator==(card32 lhs, const test_object& rhs);
-    bool operator!=(const test_object& lhs, const test_object& rhs);
-    bool operator!=(const test_object& lhs, card32 rhs);
-    bool operator!=(card32 lhs, const test_object& rhs);
-  }
+  bool operator==(const test_object& lhs, const test_object& rhs);
+  bool operator==(const test_object& lhs, card32 rhs);
+  bool operator==(card32 lhs, const test_object& rhs);
+  bool operator!=(const test_object& lhs, const test_object& rhs);
+  bool operator!=(const test_object& lhs, card32 rhs);
+  bool operator!=(card32 lhs, const test_object& rhs);
 }
