595aabfedca8ca98c5d0e667c55264d8d926eb02
Dyronix <breuck.d@buas.nl>
2024-04-23 21:58:01 +0200

    [feature/imgui] Define a verbose logging macro

--- source/2_platform/rex_directx/include/rex_directx/log.h
+++ source/2_platform/rex_directx/include/rex_directx/log.h
@@ -3,3 +3,4 @@
 #include "rex_engine/diagnostics/logging/log_macros.h"
 
 DEFINE_LOG_CATEGORY(LogDirectX, rex::LogVerbosity::Log);
+DEFINE_LOG_CATEGORY(LogDirectXVerbose, rex::LogVerbosity::Verbose);
