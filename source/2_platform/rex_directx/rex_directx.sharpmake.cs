eebddfbe8a52643fa4d9ff46745b6dd640c4cdc2
Dyronix <breuck.d@buas.nl>
2024-04-23 22:13:37 +0200

    [feature/imgui] Update rex directx project dependencies

--- source/2_platform/rex_directx/rex_directx.sharpmake.cs
+++ source/2_platform/rex_directx/rex_directx.sharpmake.cs
@@ -12,6 +12,7 @@ public class RexDirectX : PlatformProject
     GenerateTargets();
 
     string ThisFileFolder = Path.GetDirectoryName(Utils.CurrentFile());
+    SourceFilesExtensions.Add(".hlsl");
     SourceRootPath = ThisFileFolder;
   }
 
@@ -20,10 +21,17 @@ public class RexDirectX : PlatformProject
     base.SetupLibDependencies(conf, target);
 
     conf.LibraryFiles.Add("d3d12.lib");
+    conf.LibraryFiles.Add("d3dcompiler.lib");
     conf.LibraryFiles.Add("dxgi.lib");
+    conf.LibraryFiles.Add("dxguid.lib");
 
+    conf.AddPublicDependency<IMGUI>(target, DependencySetting.Default | DependencySetting.IncludeHeadersForClangtools);
+    conf.AddPublicDependency<DirectXExtra>(target);
+    conf.AddPublicDependency<DirectXTex>(target);
     conf.AddPublicDependency<RexStd>(target);
+  
     conf.AddPublicDependency<RexRendererCore>(target, DependencySetting.Default | DependencySetting.IncludeHeadersForClangtools);
+    conf.AddPublicDependency<RexDXGI>(target, DependencySetting.Default | DependencySetting.IncludeHeadersForClangtools);
     conf.AddPublicDependency<RexEngine>(target, DependencySetting.Default | DependencySetting.IncludeHeadersForClangtools);
 
     switch (target.Config)
@@ -39,5 +47,42 @@ public class RexDirectX : PlatformProject
       default:
         break;
     }
+  }
+
+  protected override void SetupPostBuildEvents(RexConfiguration conf, RexTarget target)
+  {
+    base.SetupPostBuildEvents(conf, target);
+
+    switch (ProjectGen.Settings.GraphicsAPI)
+    {
+      case ProjectGen.GraphicsAPI.DirectX12:
+        CompileShaders(conf, target);    
+        break;
+      default:
+        break;
+    }
+  }
+
+  private void CompileShaders(RexConfiguration conf, RexTarget target)
+  {
+    // Compile shaders here ...
+    //
+    // var dxc_path = "";
+    // var out_dir = "";
+    // var in_dir = "";
+    //
+    // conf.CustomBuildStep.Add(
+    //             @"$(dxc_path) /T vs_5_1 /E main /Fo $(out_dir)\unlit_vs.cso $(in_dir)",
+    //             new[] { dxc_path, out_dir, in_dir },
+    //             new[] { @"$(out_dir)\\unlit_vs.cso" },
+    //             new[] { in_dir }
+    //         );
+    //
+    // conf.CustomBuildStep.Add(
+    //             @"$(dxc_path) /T ps_5_1 /E main /Fo $(out_dir)\unlit_ps.cso $(in_dir)",
+    //             new[] { dxc_path, out_dir, in_dir },
+    //             new[] { @"$(out_dir)\\unlit_ps.cso" },
+    //             new[] { in_dir }
+    //         );
   }
 }
