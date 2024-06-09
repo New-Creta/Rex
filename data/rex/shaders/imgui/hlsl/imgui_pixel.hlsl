d71cfcb9c4a8ac4cce97f80fe82bf24e659a8d12
Nick De Breuck <nick.debreuck@outlook.com>
2024-06-09 12:30:27 +0100

    [development/gfx_multi_engine_cleanup] renaming imgui shader variables so they actually mean something

--- data/rex/shaders/imgui/hlsl/imgui_pixel.hlsl
+++ data/rex/shaders/imgui/hlsl/imgui_pixel.hlsl
@@ -4,11 +4,11 @@ struct PS_INPUT
   float4 col : COLOR0;
   float2 uv  : TEXCOORD0;
 };
-SamplerState sampler0 : register(s0);
-Texture2D texture0 : register(t0);
+SamplerState fonts_sampler : register(s0);
+Texture2D fonts_texture : register(t0);
        
 float4 main(PS_INPUT input) : SV_Target
 {
-  float4 out_col = input.col * texture0.Sample(sampler0, input.uv); 
+  float4 out_col = input.col * fonts_texture.Sample(fonts_sampler, input.uv); 
   return out_col; 
 }
