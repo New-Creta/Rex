eb983407e42136e0adff76af6fb534ef4aabf904
Nick De Breuck <nick.debreuck@outlook.com>
2024-07-05 17:14:18 +0100

    [development/materials] added json parsing capability

---
+++ source/1_engine/rex_engine/include/rex_engine/json/json.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define JSON_NOEXCEPTION
+#include "rex_engine/json/internal/json.hpp"
+
+#include "rex_engine/memory/blob.h"
+#include "rex_engine/memory/blob_view.h"
+
+namespace rex
+{
+	namespace json
+	{
+		auto parse(memory::BlobView blob)
+		{
+			auto start = blob.data();
+			auto end = blob.data() + blob.size();
+			return nlohmann::json::parse(start, end, nullptr, false);
+		}
+	}
+}
