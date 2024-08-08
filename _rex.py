63a68d2e39f53f14ec329081d34d171c726f63d6
Nick De Breuck <nick.debreuck@outlook.com>
2024-08-08 22:34:32 +0100

    [development/scenes] upgrading rex external dependencies

--- _rex.py
+++ _rex.py
@@ -25,7 +25,7 @@ try:
 except:
   rexpy_installed = False
 
-required_rexpy_version = "0.1.86"
+required_rexpy_version = "0.1.87"
 
 # all scripts are located in ~/_build/scripts path.
 # to make it easier to call these scripts wherever we need them
