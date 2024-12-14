edb343423459ca685b5397ac7ad319d8da28ee9a
Nick De Breuck <nick.debreuck@outlook.com>
2024-12-14 22:49:18 +0000

    [development/pokemon] moving process id querying out of platform specific code, well the declaration fo the function

--- source/1_engine/rex_engine/include/rex_engine/platform/win/system/win_process.h
+++ source/1_engine/rex_engine/include/rex_engine/platform/win/system/win_process.h
@@ -4,8 +4,9 @@
 #include "rex_engine/platform/win/win_types.h"
 #include "rex_std/bonus/platform/windows/handle.h"
 
+#include "rex_engine/system/process.h"
+
 namespace rex
 {
   rsl::win::handle_t current_process();
-  u32 current_process_id();
 } // namespace rex
--- source/1_engine/rex_engine/include/rex_engine/system/process.h
+++ source/1_engine/rex_engine/include/rex_engine/system/process.h
@@ -1,5 +1,10 @@
 #pragma once
 
+namespace rex
+{
+  u32 current_process_id();
+}
+
 #ifdef REX_PLATFORM_WINDOWS
   #include "rex_engine/platform/win/system/win_process.h"
 #endif
