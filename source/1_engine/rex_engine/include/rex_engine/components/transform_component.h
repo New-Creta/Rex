fb0ea3872a5d7d383c50b87a7fce036f1f8e0045
Nick De Breuck <nick.debreuck@outlook.com>
2024-12-17 22:05:47 +0000

    [development/pokemon] finished writing unit tests for transform component

--- source/1_engine/rex_engine/include/rex_engine/components/transform_component.h
+++ source/1_engine/rex_engine/include/rex_engine/components/transform_component.h
@@ -17,43 +17,49 @@ namespace rex
 		struct TransformComponent
 		{
 			glm::vec3 translation{};							// Where in the world is it located
-			glm::vec3 rotation{};								// What's the rotation of the object
+			glm::vec3 rotation_euler{};						// What's the rotation of the object
 			glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);	// What's the scale of the object
 			
 			// Convert the transform of an object to a world matrix
 			glm::mat4 world_mat() const
 			{
-				glm::mat4 rotation_mat = glm::toMat4(glm::quat(rotation));
+				glm::mat4 rotation_mat = glm::toMat4(glm::quat(glm::radians(rotation_euler)));
 
 				glm::mat4 world = glm::translate(glm::mat4(1.0f), translation) * rotation_mat * glm::scale(glm::mat4(1.0f), scale);
 				return glm::transpose(world);
 			}
 
-			void rotate_x(rsl::deg_angle angle)
+			TransformComponent& rotate_x(rsl::deg_angle angle)
 			{
-
+				rotation_euler.x += angle.get();
+				return *this;
 			}
-			void rotate_x(rsl::rad_angle angle)
+			TransformComponent& rotate_x(rsl::rad_angle angle)
 			{
-
-			}
-
-			void rotate_y(rsl::deg_angle angle)
-			{
-
-			}
-			void rotate_y(rsl::rad_angle angle)
-			{
-
+				rotation_euler.x += angle.to_deg();
+				return *this;
 			}
 
-			void rotate_z(rsl::deg_angle angle)
+			TransformComponent& rotate_y(rsl::deg_angle angle)
 			{
-
+				rotation_euler.y += angle.get();
+				return *this;
 			}
-			void rotate_z(rsl::rad_angle angle)
+			TransformComponent& rotate_y(rsl::rad_angle angle)
 			{
+				rotation_euler.y += angle.to_deg();
+				return *this;
+			}
 
+			TransformComponent& rotate_z(rsl::deg_angle angle)
+			{
+				rotation_euler.z += angle.get();
+				return *this;
+			}
+			TransformComponent& rotate_z(rsl::rad_angle angle)
+			{
+				rotation_euler.z += angle.to_deg();
+				return *this;
 			}
 		};
 	}
