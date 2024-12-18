5ab7e2c9cb950fbfe6f26e19e37d610bc283fa0f
Nick De Breuck <nick.debreuck@outlook.com>
2024-12-18 21:57:30 +0000

    [development/pokemon] adding unit test files for all supported events. They're all tested in the same way but that's the point, none of them should behave in a special way

---
+++ tests/1_engine/rex_engine_unit_test/src/event_system/events/input/test_mouse_down.cpp
@@ -0,0 +1,28 @@
+#include "rex_unit_test/rex_catch2.h"
+
+#include "rex_engine/event_system/events/input/mouse_down.h"
+#include "rex_engine/event_system/event_system.h"
+
+TEST_CASE("Quit App Event")
+{
+  s32 num_quit_events_fired = 0;
+  rsl::string_view quit_msg;
+  auto subscription = rex::event_system().subscribe<rex::MouseDown>([&num_quit_events_fired, &quit_msg](const rex::MouseDown& evt)
+    {
+      ++num_quit_events_fired;
+      REX_CHECK(evt.reason() == quit_msg);
+    });
+
+  // Immediate event fire
+  quit_msg = "Test Quit Event";
+  rex::event_system().fire_event(rex::MouseDown(quit_msg));
+  REX_CHECK(num_quit_events_fired == 1);
+
+  // Queued event fire
+  quit_msg = "Test Quit Event 2";
+  rex::event_system().enqueue_event(rex::MouseDown(quit_msg));
+  REX_CHECK(num_quit_events_fired == 1);
+
+  rex::event_system().dispatch_queued_events();
+  REX_CHECK(num_quit_events_fired == 2);
+}
