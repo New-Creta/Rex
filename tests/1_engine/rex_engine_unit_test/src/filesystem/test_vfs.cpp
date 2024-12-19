92733d9b87f200c73b58742ed0d7066ab0c0ef40
Nick De Breuck <nick.debreuck@outlook.com>
2024-12-19 00:24:57 +0000

    [development/pokemon] prepping the vfs unit tests, not implemented yet though

---
+++ tests/1_engine/rex_engine_unit_test/src/filesystem/test_vfs.cpp
@@ -0,0 +1,81 @@
+#include "rex_unit_test/rex_catch2.h"
+
+TEST_CASE("Test VFS - root")
+{
+
+}
+TEST_CASE("Test VFS - engine root")
+{
+
+}
+TEST_CASE("Test VFS - project root")
+{
+
+}
+TEST_CASE("Test VFS - sessions root")
+{
+
+}
+TEST_CASE("Test VFS - project sessions root")
+{
+
+}TEST_CASE("Test VFS - session data root")
+{
+
+}
+TEST_CASE("Test VFS - init")
+{
+
+}
+TEST_CASE("Test VFS - mount")
+{
+
+}
+TEST_CASE("Test VFS - mount for session")
+{
+
+}
+TEST_CASE("Test VFS - read file")
+{
+
+}
+TEST_CASE("Test VFS - read file async")
+{
+
+}
+TEST_CASE("Test VFS - save to file")
+{
+
+}
+TEST_CASE("Test VFS - create dir")
+{
+
+}
+TEST_CASE("Test VFS - create dirs")
+{
+
+}
+TEST_CASE("Test VFS - exists")
+{
+
+}
+TEST_CASE("Test VFS - is dir")
+{
+
+}
+TEST_CASE("Test VFS - is file")
+{
+
+}
+TEST_CASE("Test VFS - abs path")
+{
+
+}
+TEST_CASE("Test VFS - mount path")
+{
+
+}
+TEST_CASE("Test VFS - files in dir")
+{
+
+}
