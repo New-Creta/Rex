a405b826f04b6f06030ae2bd9b95b10a5a590ba3
Nick De Breuck <nick.debreuck@outlook.com>
2025-02-16 23:01:39 +0000

    [development/pokemon] fixing data type unit tests now that the implementation has changed

--- tests/1_engine/rex_engine_unit_test/src/engine/test_data_type.cpp
+++ tests/1_engine/rex_engine_unit_test/src/engine/test_data_type.cpp
@@ -4,36 +4,36 @@
 
 TEST_CASE("TEST - Data Type - construction from type")
 {
-	REX_CHECK(rex::data_type<s8>().get() == rex::DataType::Value::Int8);
-	REX_CHECK(rex::data_type<s16>().get() == rex::DataType::Value::Int16);
-	REX_CHECK(rex::data_type<s32>().get() == rex::DataType::Value::Int32);
-	REX_CHECK(rex::data_type<s64>().get() == rex::DataType::Value::Int64);
+	REX_CHECK(rex::data_type<s8>() == rex::DataType::Int8);
+	REX_CHECK(rex::data_type<s16>() == rex::DataType::Int16);
+	REX_CHECK(rex::data_type<s32>() == rex::DataType::Int32);
+	REX_CHECK(rex::data_type<s64>() == rex::DataType::Int64);
 
-	REX_CHECK(rex::data_type<u8>().get() == rex::DataType::Value::UnsignedInt8);
-	REX_CHECK(rex::data_type<u16>().get() == rex::DataType::Value::UnsignedInt16);
-	REX_CHECK(rex::data_type<u32>().get() == rex::DataType::Value::UnsignedInt32);
-	REX_CHECK(rex::data_type<u64>().get() == rex::DataType::Value::UnsignedInt64);
+	REX_CHECK(rex::data_type<u8>() == rex::DataType::UnsignedInt8);
+	REX_CHECK(rex::data_type<u16>() == rex::DataType::UnsignedInt16);
+	REX_CHECK(rex::data_type<u32>() == rex::DataType::UnsignedInt32);
+	REX_CHECK(rex::data_type<u64>() == rex::DataType::UnsignedInt64);
 
-	REX_CHECK(rex::data_type<f32>().get() == rex::DataType::Value::Float32);
-	REX_CHECK(rex::data_type<f64>().get() == rex::DataType::Value::Float64);
+	REX_CHECK(rex::data_type<f32>() == rex::DataType::Float32);
+	REX_CHECK(rex::data_type<f64>() == rex::DataType::Float64);
 
-	REX_CHECK(rex::data_type<bool>().get() == rex::DataType::Value::Bool);
+	REX_CHECK(rex::data_type<bool>() == rex::DataType::Bool);
 
-	REX_CHECK(rex::data_type<glm::mat3>().get() == rex::DataType::Value::Mat3);
-	REX_CHECK(rex::data_type<glm::mat4>().get() == rex::DataType::Value::Mat4);
+	REX_CHECK(rex::data_type<glm::mat3>() == rex::DataType::Mat3);
+	REX_CHECK(rex::data_type<glm::mat4>() == rex::DataType::Mat4);
 
-	REX_CHECK(rex::data_type<glm::vec2>().get() == rex::DataType::Value::Vec2);
-	REX_CHECK(rex::data_type<glm::vec3>().get() == rex::DataType::Value::Vec3);
-	REX_CHECK(rex::data_type<glm::vec4>().get() == rex::DataType::Value::Vec4);
+	REX_CHECK(rex::data_type<glm::vec2>() == rex::DataType::Vec2);
+	REX_CHECK(rex::data_type<glm::vec3>() == rex::DataType::Vec3);
+	REX_CHECK(rex::data_type<glm::vec4>() == rex::DataType::Vec4);
 
-	REX_CHECK(rex::data_type<glm::ivec2>().get() == rex::DataType::Value::Ivec2);
-	REX_CHECK(rex::data_type<glm::ivec3>().get() == rex::DataType::Value::Ivec3);
-	REX_CHECK(rex::data_type<glm::ivec4>().get() == rex::DataType::Value::Ivec4);
+	REX_CHECK(rex::data_type<glm::ivec2>() == rex::DataType::Ivec2);
+	REX_CHECK(rex::data_type<glm::ivec3>() == rex::DataType::Ivec3);
+	REX_CHECK(rex::data_type<glm::ivec4>() == rex::DataType::Ivec4);
 
-	REX_CHECK(rex::data_type<glm::uvec2>().get() == rex::DataType::Value::Uvec2);
-	REX_CHECK(rex::data_type<glm::uvec3>().get() == rex::DataType::Value::Uvec3);
-	REX_CHECK(rex::data_type<glm::uvec4>().get() == rex::DataType::Value::Uvec4);
+	REX_CHECK(rex::data_type<glm::uvec2>() == rex::DataType::Uvec2);
+	REX_CHECK(rex::data_type<glm::uvec3>() == rex::DataType::Uvec3);
+	REX_CHECK(rex::data_type<glm::uvec4>() == rex::DataType::Uvec4);
 
-	REX_CHECK(rex::data_type<rsl::string>().get() == rex::DataType::Value::String);
-	REX_CHECK(rex::data_type<rsl::string_view>().get() == rex::DataType::Value::String);
+	REX_CHECK(rex::data_type<rsl::string>() == rex::DataType::String);
+	REX_CHECK(rex::data_type<rsl::string_view>() == rex::DataType::String);
 }
